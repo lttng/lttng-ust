04203c909b39fc97bc6d31d8ad55a9d4e5863613
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-06-15 11:46:15 -0400

    ust/localerr.h: fix copy-paste error

--- ust/localerr.h
+++ ust/localerr.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-#define DBG(fmt, args...) fprintf(stderr, "ustd: " fmt "\n", ## args); fflush(stderr)
-#define WARN(fmt, args...) fprintf(stderr, "ustd: WARNING: " fmt "\n", ## args); fflush(stderr)
-#define ERR(fmt, args...) fprintf(stderr, "ustd: ERROR: " fmt "\n", ## args); fflush(stderr)
+#define DBG(fmt, args...) fprintf(stderr, "ust: " fmt "\n", ## args); fflush(stderr)
+#define WARN(fmt, args...) fprintf(stderr, "ust: WARNING: " fmt "\n", ## args); fflush(stderr)
+#define ERR(fmt, args...) fprintf(stderr, "ust: ERROR: " fmt "\n", ## args); fflush(stderr)
 #define PERROR(a) perror(a)
 
 #endif /* LOCALERR_H */
