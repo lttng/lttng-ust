6dd969b5dd702909b8eb827e850c851053774055
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-06-15 13:03:25 -0400

    reorganize test programs into tests/ directory

---
+++ tests/hello2/hello2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "marker.h"
+int main()
+{
+	int result;
+	int i;
+
+//	sleep(1);
+
+	printf("Hello, World!\n");
+
+	for(i=0; i<500; i++) {
+		trace_mark(ust, bar, "str %d", i);
+		trace_mark(ust, bar2, "number1 %d number2 %d", (int)53, (int)9800);
+		usleep(20);
+	}
+
+	return 0;
+}
+MARKER_LIB;
