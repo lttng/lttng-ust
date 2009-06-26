09dcbc3d4aac11ca707228eef9ad56295eeca0f8
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-06-25 22:36:27 -0400

    enhance hello2

--- tests/hello2/hello2.c
+++ tests/hello2/hello2.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+
 #include "marker.h"
 int main()
 {
 	int i;
+	struct timespec tv;
+	struct timespec rem;
+	int result;
 
-//	sleep(1);
+	tv.tv_sec = 1;
+	tv.tv_nsec = 0;
+
+	do {
+		result = nanosleep(&tv, &tv);
+	} while(result == -1 && errno == EINTR);
 
 	printf("Hello, World!\n");
 
 	for(i=0; i<500; i++) {
 		trace_mark(ust, bar, "str %d", i);
 		trace_mark(ust, bar2, "number1 %d number2 %d", (int)53, (int)9800);
-		usleep(20);
 	}
 
 	return 0;
