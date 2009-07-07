22cb38fbb2b49a37488e09c836e19f924d1ebbc8
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-07-07 12:18:21 -0400

    change some licences to LGPL

--- libust/serialize.c
+++ libust/serialize.c
@@ -3,7 +3,20 @@
  *
  * Copyright Mathieu Desnoyers, March 2007.
  *
- * Licensed under the GPLv2.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
  *
  * See this discussion about weirdness about passing va_list and then va_list to
  * functions. (related to array argument passing). va_list seems to be
