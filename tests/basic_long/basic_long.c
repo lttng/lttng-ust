5f04cdcbbea9d600b3f325dd739b217ab2a8f78a
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-09-08 13:56:39 -0400

    add basic_long test program

---
+++ tests/basic_long/basic_long.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "marker.h"
+
+
+int main()
+{
+	printf("Basic test program\n");
+
+	for(;;) {
+		trace_mark(ust, bar, "str %s", "FOOBAZ");
+		trace_mark(ust, bar2, "number1 %d number2 %d", 53, 9800);
+		usleep(1000000);
+	}
+
+	return 0;
+}
+
+MARKER_LIB;
