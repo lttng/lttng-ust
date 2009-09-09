e91e64b6dd6bc59347a88d5154bd89c3d0c82acd
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-09-09 11:53:01 -0400

    usterr: fix PERROR

--- share/usterr.h
+++ share/usterr.h
@@ -21,7 +21,7 @@
 #define WARN(fmt, args...) fprintf(stderr, UST_STR_COMPONENT ": Warning: " fmt "\n", ## args); fflush(stderr)
 #define ERR(fmt, args...) fprintf(stderr, UST_STR_COMPONENT ": Error: " fmt "\n", ## args); fflush(stderr)
 #define BUG(fmt, args...) fprintf(stderr, UST_STR_COMPONENT ": BUG: " fmt "\n", ## args); fflush(stderr)
-#define PERROR(call) perror("ust: ERROR: " call)
+#define PERROR(call) perror(UST_STR_COMPONENT ": Error: " call)
 
 #define BUG_ON(condition) do { if (unlikely(condition)) ERR("condition not respected (BUG)"); } while(0)
 #define WARN_ON(condition) do { if (unlikely(condition)) WARN("condition not respected on line %s:%d", __FILE__, __LINE__); } while(0)
