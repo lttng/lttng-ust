5ab4c5cbc13fe373def19dbfcbf669a60b7c71a9
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-10-27 19:14:55 -0400

    java: fixes

---
+++ java/UST.c
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <ust/marker.h>
+
+JNIEXPORT void JNICALL Java_UST_ust_1java_1event (JNIEnv *env, jobject jobj, jstring ev_name, jstring args)
+{
+	jboolean iscopy;
+	const char *ev_name_cstr = (*env)->GetStringUTFChars(env, ev_name, &iscopy);
+	const char *args_cstr = (*env)->GetStringUTFChars(env, args, &iscopy);
+
+	trace_mark(ust, java_event, "name %s args %s", ev_name_cstr, args_cstr);
+}
+
+MARKER_LIB
