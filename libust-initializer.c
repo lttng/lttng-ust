25123621e72daf757b53fe8b5f28130a98016988
Jan Blunck <jblunck@suse.de>
2009-11-05 18:13:18 +0100

    Add linker script and libust initializer object
    This adds a libust initializer object which gets linked automagically if
    you link against libust.so, which is a linker skript from now on. The
    object file just ensures that the libust is initialized properly without
    forcing the user of the library to care about calling MARKER_LIB and
    TRACEPOINT_LIB explicitly.
    
    Signed-off-by: Jan Blunck <jblunck@suse.de>

---
+++ libust-initializer.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009 Novell Inc.
+ *
+ * Author: Jan Blunck <jblunck@suse.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2.1 as
+ * published by the Free  Software Foundation.
+ */
+
+#include <ust/marker.h>
+#include <ust/tracepoint.h>
+
+MARKER_LIB;
+TRACEPOINT_LIB;
