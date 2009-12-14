d57c0d75a247da5dffd8b07a9717958f6cb76e64
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2009-12-14 02:10:01 -0500

    Add dummy marker and tracepoint to libust-initializer
    Without these, the __start... and __stop... symbols are
    not defined, and compilation fails with an obscure relocation
    message. A more elegant solution should be found.

--- libust-initializer.c
+++ libust-initializer.c
@@ -11,5 +11,21 @@
 #include <ust/marker.h>
 #include <ust/tracepoint.h>
 
+/* FIXME: We have to define at least one trace_mark and
+ * one tracepoint here. If we don't, the __start... and
+ * __stop... symbols won't be defined and the constructors
+ * won't be compilable. We should find a linker trick to
+ * avoid this.
+ */
+
+DECLARE_TRACE(ust_dummytp, TPPROTO(void), TPARGS());
+DEFINE_TRACE(ust_dummytp);
+
+void dummy_libust_initializer_func(void)
+{
+	trace_mark(ust, dummymark, MARK_NOARGS);
+	trace_ust_dummytp();
+}
+
 MARKER_LIB;
 TRACEPOINT_LIB;
