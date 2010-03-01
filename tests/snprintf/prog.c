69f1f6526a6d40556ed3dcb50cdc199f45793b2f
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2010-03-01 17:45:47 -0500

    move snprintf test suite to tests/snprintf/ and add test

--- tests/snprintf/prog.c
+++ tests/snprintf/prog.c
@@ -7,8 +7,8 @@ int main()
 
 	char *expected;
 
-	expected = "header 9999, hello, 003";
-	ust_safe_snprintf(buf, 99, "header %d, %s, %03d", 9999, "hello", 3);
+	expected = "header 9999, hello, 005, '    9'";
+	ust_safe_snprintf(buf, 99, "header %d, %s, %03d, '%3$*d'", 9999, "hello", 5, 9);
 	if(strcmp(buf, expected) != 0) {
 		printf("Error: expected \"%s\" and got \"%s\"\n", expected, buf);
 		return 1;
