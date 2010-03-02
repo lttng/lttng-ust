e6ef41119258799ca3720c5f2c5af4db327b2720
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2010-03-02 15:04:07 -0500

    tests: add test-libmallocwrap

---
+++ tests/test-libmallocwrap/prog.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdlib.h>
+
+#define N_ITER 1000
+
+int main()
+{
+	int i;
+	const char teststr[] = "Hello World! 1234567890abc";
+	void *ptrs[N_ITER];
+
+	for(i=0; i<N_ITER; i++) {
+		ptrs[i] = malloc(i+1000);
+
+		memcpy(ptrs[i], teststr, sizeof(teststr));
+
+		if(i%2 == 0) {
+			free(ptrs[i]);
+		}
+	}
+
+	for(i=0; i<N_ITER; i++) {
+		if(i%2 == 1)
+			free(ptrs[i]);
+	}
+
+	return 0;
+}
