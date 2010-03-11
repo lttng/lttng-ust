c7dc133cff3db7336bb97fcb57c2dec559942518
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2010-03-11 18:38:27 -0500

    make libustcomm into a static lib

--- libust/buffers.c
+++ libust/buffers.c
@@ -49,26 +49,24 @@ static int get_n_cpus(void)
 	int result;
 	static int n_cpus = 0;
 
-	if(n_cpus) {
-		return n_cpus;
+	if(!n_cpus) {
+		/* On Linux, when some processors are offline
+		 * _SC_NPROCESSORS_CONF counts the offline
+		 * processors, whereas _SC_NPROCESSORS_ONLN
+		 * does not. If we used _SC_NPROCESSORS_ONLN,
+		 * getcpu() could return a value greater than
+		 * this sysconf, in which case the arrays
+		 * indexed by processor would overflow.
+		 */
+		result = sysconf(_SC_NPROCESSORS_CONF);
+		if(result == -1) {
+			return -1;
+		}
+
+		n_cpus = result;
 	}
 
-	/* On Linux, when some processors are offline
-	 * _SC_NPROCESSORS_CONF counts the offline
-	 * processors, whereas _SC_NPROCESSORS_ONLN
-	 * does not. If we used _SC_NPROCESSORS_ONLN,
-	 * getcpu() could return a value greater than
-	 * this sysconf, in which case the arrays
-	 * indexed by processor would overflow.
-	 */
-	result = sysconf(_SC_NPROCESSORS_CONF);
-	if(result == -1) {
-		return -1;
-	}
-
-	n_cpus = result;
-
-	return result;
+	return n_cpus;
 }
 
 /* _ust_buffers_write()
