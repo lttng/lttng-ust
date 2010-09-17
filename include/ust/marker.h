f91aa7301fe975dc6b1d5583f7ffb6c4d2dbf325
David Goulet <david.goulet@polymtl.ca>
2010-09-17 21:41:11 +0200

    Syntax cleanup
    Replace spaces by tabs and remove trailing white spaces
    
    Signed-off-by: David Goulet <david.goulet@polymtl.ca>
    Acked-by: Nils Carlson <nils.carlson@ericsson.com>

--- include/ust/marker.h
+++ include/ust/marker.h
@@ -64,7 +64,7 @@ struct marker {
 	const char *format;	/* Marker format string, describing the
 				 * variable argument list.
 				 */
- 	DEFINE_IMV(char, state);/* Immediate value state. */
+	DEFINE_IMV(char, state);/* Immediate value state. */
 	char ptype;		/* probe type : 0 : single, 1 : multi */
 				/* Probe wrapper */
 	u16 channel_id;		/* Numeric channel identifier, dynamic */
@@ -89,47 +89,47 @@ struct marker {
 		 * because it must not require %-sign escaping, as we most certainly		\
 		 * have some %-signs in the format string.					\
 		 */										\
-		asm volatile ( 									\
-		     /* We only define these symbols if they have not yet been defined. Indeed, \
-		      * if two markers with the same channel/name are on the same line, they	\
-		      * will try to create the same symbols, resulting in a conflict. This	\
-		      * is not unusual as it can be the result of function inlining.		\
-		      */									\
-		     ".ifndef __mstrtab_" __stringify(channel) "_" __stringify(name) "_channel_" __stringify(unique) "\n\t"	\
-		     /*".section __markers_strings\n\t"*/					\
-		     ".section __markers_strings,\"aw\",@progbits\n\t"				\
-		     "__mstrtab_" __stringify(channel) "_" __stringify(name) "_channel_" __stringify(unique) ":\n\t"	\
-		     ".string \"" __stringify(channel) "\"\n\t"					\
-		     "__mstrtab_" __stringify(channel) "_" __stringify(name) "_name_" __stringify(unique) ":\n\t"	\
-		     ".string \"" __stringify(name) "\"\n\t"						\
-		     "__mstrtab_" __stringify(channel) "_" __stringify(name) "_format_" __stringify(unique) ":\n\t"	\
-		     ".string " "\"" format "\"" "\n\t"						\
-		     ".previous\n\t"								\
-		     ".endif\n\t"								\
+		asm volatile (									\
+			/* We only define these symbols if they have not yet been defined. Indeed, \
+			 * if two markers with the same channel/name are on the same line, they	\
+			 * will try to create the same symbols, resulting in a conflict. This	\
+			 * is not unusual as it can be the result of function inlining.		\
+			 */									\
+			".ifndef __mstrtab_" __stringify(channel) "_" __stringify(name) "_channel_" __stringify(unique) "\n\t"	\
+			/*".section __markers_strings\n\t"*/					\
+			".section __markers_strings,\"aw\",@progbits\n\t"				\
+			"__mstrtab_" __stringify(channel) "_" __stringify(name) "_channel_" __stringify(unique) ":\n\t"	\
+			".string \"" __stringify(channel) "\"\n\t"					\
+			"__mstrtab_" __stringify(channel) "_" __stringify(name) "_name_" __stringify(unique) ":\n\t"	\
+			".string \"" __stringify(name) "\"\n\t"						\
+			"__mstrtab_" __stringify(channel) "_" __stringify(name) "_format_" __stringify(unique) ":\n\t"	\
+			".string " "\"" format "\"" "\n\t"						\
+			".previous\n\t"								\
+			".endif\n\t"								\
 		);										\
 		asm volatile (									\
-		     /*".section __markers\n\t"*/ \
-		     ".section __markers,\"aw\",@progbits\n\t"					\
-		     ".balign 8\n\t" 								\
-		     "2:\n\t" \
-		     _ASM_PTR "(__mstrtab_" __stringify(channel) "_" __stringify(name) "_channel_" __stringify(unique) ")\n\t" /* channel string */ \
-		     _ASM_PTR "(__mstrtab_" __stringify(channel) "_" __stringify(name) "_name_" __stringify(unique) ")\n\t" /* name string */ \
-		     _ASM_PTR "(__mstrtab_" __stringify(channel) "_" __stringify(name) "_format_" __stringify(unique) ")\n\t" /* format string */ \
-		     ".byte 0\n\t" /* state imv */						\
-		     ".byte 0\n\t" /* ptype */							\
-		     ".word 0\n\t" /* channel_id */						\
-		     ".word 0\n\t" /* event_id */						\
-		     ".balign " __stringify(__WORDSIZE) " / 8\n\t" /* alignment */			\
-		     _ASM_PTR "(marker_probe_cb)\n\t" /* call */				\
-		     _ASM_PTR "(__mark_empty_function)\n\t" /* marker_probe_closure single.field1 */ \
-		     _ASM_PTR "0\n\t" /* marker_probe_closure single.field2 */			\
-		     _ASM_PTR "0\n\t" /* marker_probe_closure *multi */				\
-		     _ASM_PTR "0\n\t" /* tp_name */						\
-		     _ASM_PTR "0\n\t" /* tp_cb */						\
-		     _ASM_PTR "(1f)\n\t" /* location */						\
-		     ".previous\n\t"								\
-		     "1:\n\t"									\
-		     ARCH_COPY_ADDR("%[outptr]")						\
+			/*".section __markers\n\t"*/ \
+			".section __markers,\"aw\",@progbits\n\t"					\
+			".balign 8\n\t"								\
+			"2:\n\t" \
+			_ASM_PTR "(__mstrtab_" __stringify(channel) "_" __stringify(name) "_channel_" __stringify(unique) ")\n\t" /* channel string */ \
+			_ASM_PTR "(__mstrtab_" __stringify(channel) "_" __stringify(name) "_name_" __stringify(unique) ")\n\t" /* name string */ \
+			_ASM_PTR "(__mstrtab_" __stringify(channel) "_" __stringify(name) "_format_" __stringify(unique) ")\n\t" /* format string */ \
+			".byte 0\n\t" /* state imv */						\
+			".byte 0\n\t" /* ptype */							\
+			".word 0\n\t" /* channel_id */						\
+			".word 0\n\t" /* event_id */						\
+			".balign " __stringify(__WORDSIZE) " / 8\n\t" /* alignment */			\
+			_ASM_PTR "(marker_probe_cb)\n\t" /* call */				\
+			_ASM_PTR "(__mark_empty_function)\n\t" /* marker_probe_closure single.field1 */ \
+			_ASM_PTR "0\n\t" /* marker_probe_closure single.field2 */			\
+			_ASM_PTR "0\n\t" /* marker_probe_closure *multi */				\
+			_ASM_PTR "0\n\t" /* tp_name */						\
+			_ASM_PTR "0\n\t" /* tp_cb */						\
+			_ASM_PTR "(1f)\n\t" /* location */						\
+			".previous\n\t"								\
+			"1:\n\t"									\
+			ARCH_COPY_ADDR("%[outptr]")						\
 		: [outptr] "=r" (m) );								\
 												\
 		save_registers(&regs)
@@ -148,12 +148,11 @@ struct marker {
 		static struct marker GET_MARKER(channel, name)		\
 		__attribute__((section("__markers"), aligned(8))) =	\
 		{ __mstrtab_##channel##_##name,				\
-		  &__mstrtab_##channel##_##name[sizeof(#channel)],	\
-		  &__mstrtab_##channel##_##name[sizeof(#channel) +	\
-						sizeof(#name)],		\
-		  0, 0, 0, 0, marker_probe_cb,				\
-		  { __mark_empty_function, NULL},			\
-		  NULL, tp_name_str, tp_cb }
+			&__mstrtab_##channel##_##name[sizeof(#channel)],	\
+			&__mstrtab_##channel##_##name[sizeof(#channel) + sizeof(#name)],		\
+			0, 0, 0, 0, marker_probe_cb,				\
+			{ __mark_empty_function, NULL},			\
+			NULL, tp_name_str, tp_cb }
 
 /*
  * Make sure the alignment of the structure in the __markers section will
