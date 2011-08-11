67c5b804ac870bdc3b262c0cf6cbf117f3e6bc1e
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-08-11 19:28:06 -0400

    Merge copy of lttng-sessiond-comm
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ include/lttng-sessiond-comm.h
@@ -0,0 +1,210 @@
+#ifndef _LTTNG_SESSIOND_COMM_H
+#define _LTTNG_SESSIOND_COMM_H
+
+/*
+ * Copyright (C) 2011 - David Goulet <david.goulet@polymtl.ca>
+ *                      Julien Desfossez <julien.desfossez@polymtl.ca>
+ *                      Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; only version 2
+ * of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/*
+ * This header is meant for liblttng and libust internal use ONLY.
+ * These declarations should NOT be considered stable API.
+ */
+
+#include <limits.h>
+#include <lttng/lttng.h>
+
+#define LTTNG_RUNDIR                        "/var/run/lttng"
+
+/* Default unix socket path */
+#define DEFAULT_GLOBAL_CLIENT_UNIX_SOCK     LTTNG_RUNDIR "/client-ltt-sessiond"
+#define DEFAULT_GLOBAL_APPS_UNIX_SOCK       LTTNG_RUNDIR "/apps-ltt-sessiond"
+#define DEFAULT_HOME_APPS_UNIX_SOCK         "%s/.apps-ltt-sessiond"
+#define DEFAULT_HOME_CLIENT_UNIX_SOCK       "%s/.client-ltt-sessiond"
+
+/* Queue size of listen(2) */
+#define MAX_LISTEN 10
+
+/* Get the error code index from 0 since
+ * LTTCOMM_OK start at 1000
+ */
+#define LTTCOMM_ERR_INDEX(code) (code - LTTCOMM_OK)
+
+enum lttcomm_sessiond_command {
+	/* Tracer command */
+	LTTNG_ADD_CONTEXT,
+	LTTNG_CALIBRATE,
+	LTTNG_DISABLE_CHANNEL,
+	LTTNG_DISABLE_EVENT,
+	LTTNG_DISABLE_ALL_EVENT,
+	LTTNG_ENABLE_CHANNEL,
+	LTTNG_ENABLE_EVENT,
+	LTTNG_ENABLE_ALL_EVENT,
+	/* Session daemon command */
+	LTTNG_CREATE_SESSION,
+	LTTNG_DESTROY_SESSION,
+	LTTNG_LIST_CHANNELS,
+	LTTNG_LIST_DOMAINS,
+	LTTNG_LIST_EVENTS,
+	LTTNG_LIST_SESSIONS,
+	LTTNG_LIST_TRACEPOINTS,
+	LTTNG_START_TRACE,
+	LTTNG_STOP_TRACE,
+};
+
+/*
+ * lttcomm error code.
+ */
+enum lttcomm_return_code {
+	LTTCOMM_OK = 1000,				/* Ok */
+	LTTCOMM_ERR,					/* Unknown Error */
+	LTTCOMM_UND,					/* Undefine command */
+	LTTCOMM_NOT_IMPLEMENTED,        /* Command not implemented */
+	LTTCOMM_UNKNOWN_DOMAIN,         /* Tracing domain not known */
+	LTTCOMM_ALLOC_FAIL,				/* Trace allocation fail */
+	LTTCOMM_NO_SESSION,				/* No session found */
+	LTTCOMM_CREATE_FAIL,			/* Create trace fail */
+	LTTCOMM_SESSION_FAIL,			/* Create session fail */
+	LTTCOMM_START_FAIL,				/* Start tracing fail */
+	LTTCOMM_STOP_FAIL,				/* Stop tracing fail */
+	LTTCOMM_LIST_FAIL,				/* Listing apps fail */
+	LTTCOMM_NO_APPS,				/* No traceable application */
+	LTTCOMM_SESS_NOT_FOUND,			/* Session name not found */
+	LTTCOMM_NO_TRACE,				/* No trace exist */
+	LTTCOMM_FATAL,					/* Session daemon had a fatal error */
+	LTTCOMM_NO_TRACEABLE,			/* Error for non traceable app */
+	LTTCOMM_SELECT_SESS,			/* Must select a session */
+	LTTCOMM_EXIST_SESS,				/* Session name already exist */
+	LTTCOMM_NO_EVENT,				/* No event found */
+	LTTCOMM_KERN_NA,				/* Kernel tracer unavalable */
+	LTTCOMM_KERN_EVENT_EXIST,       /* Kernel event already exists */
+	LTTCOMM_KERN_SESS_FAIL,			/* Kernel create session failed */
+	LTTCOMM_KERN_CHAN_FAIL,			/* Kernel create channel failed */
+	LTTCOMM_KERN_CHAN_NOT_FOUND,	/* Kernel channel not found */
+	LTTCOMM_KERN_CHAN_DISABLE_FAIL, /* Kernel disable channel failed */
+	LTTCOMM_KERN_CHAN_ENABLE_FAIL,  /* Kernel enable channel failed */
+	LTTCOMM_KERN_CONTEXT_FAIL,      /* Kernel add context failed */
+	LTTCOMM_KERN_ENABLE_FAIL,		/* Kernel enable event failed */
+	LTTCOMM_KERN_DISABLE_FAIL,		/* Kernel disable event failed */
+	LTTCOMM_KERN_META_FAIL,			/* Kernel open metadata failed */
+	LTTCOMM_KERN_START_FAIL,		/* Kernel start trace failed */
+	LTTCOMM_KERN_STOP_FAIL,			/* Kernel stop trace failed */
+	LTTCOMM_KERN_CONSUMER_FAIL,		/* Kernel consumer start failed */
+	LTTCOMM_KERN_STREAM_FAIL,		/* Kernel create stream failed */
+	LTTCOMM_KERN_DIR_FAIL,			/* Kernel trace directory creation failed */
+	LTTCOMM_KERN_DIR_EXIST,			/* Kernel trace directory exist */
+	LTTCOMM_KERN_NO_SESSION,		/* No kernel session found */
+	LTTCOMM_KERN_LIST_FAIL,			/* Kernel listing events failed */
+	KCONSUMERD_COMMAND_SOCK_READY,	/* when kconsumerd command socket ready */
+	KCONSUMERD_SUCCESS_RECV_FD,		/* success on receiving fds */
+	KCONSUMERD_ERROR_RECV_FD,		/* error on receiving fds */
+	KCONSUMERD_POLL_ERROR,			/* Error in polling thread in kconsumerd */
+	KCONSUMERD_POLL_NVAL,			/* Poll on closed fd */
+	KCONSUMERD_POLL_HUP,			/* All fds have hungup */
+	KCONSUMERD_EXIT_SUCCESS,		/* kconsumerd exiting normally */
+	KCONSUMERD_EXIT_FAILURE,		/* kconsumerd exiting on error */
+	KCONSUMERD_OUTFD_ERROR,			/* error opening the tracefile */
+	KCONSUMERD_SPLICE_EBADF,		/* EBADF from splice(2) */
+	KCONSUMERD_SPLICE_EINVAL,		/* EINVAL from splice(2) */
+	KCONSUMERD_SPLICE_ENOMEM,		/* ENOMEM from splice(2) */
+	KCONSUMERD_SPLICE_ESPIPE,		/* ESPIPE from splice(2) */
+	/* MUST be last element */
+	LTTCOMM_NR,						/* Last element */
+};
+
+/*
+ * Data structure received from lttng client to session daemon.
+ */
+struct lttcomm_session_msg {
+	uint32_t cmd_type;    /* enum lttcomm_sessiond_command */
+	struct lttng_session session;
+	struct lttng_domain domain;
+	union {
+		struct {
+			char channel_name[NAME_MAX];
+			char name[NAME_MAX];
+		} disable;
+		/* Event data */
+		struct {
+			char channel_name[NAME_MAX];
+			struct lttng_event event;
+		} enable;
+		/* Create channel */
+		struct {
+			struct lttng_channel chan;
+		} channel;
+		/* Context */
+		struct {
+			char channel_name[NAME_MAX];
+			char event_name[NAME_MAX];
+			struct lttng_event_context ctx;
+		} context;
+		/* List */
+		struct {
+			char channel_name[NAME_MAX];
+		} list;
+		struct lttng_calibrate calibrate;
+	} u;
+};
+
+/*
+ * Data structure for the response from sessiond to the lttng client.
+ */
+struct lttcomm_lttng_msg {
+	uint32_t cmd_type;   /* enum lttcomm_sessiond_command */
+	uint32_t ret_code;   /* enum lttcomm_return_code */
+	uint32_t pid;        /* pid_t */
+	uint32_t data_size;
+	/* Contains: trace_name + data */
+	char payload[];
+};
+
+/*
+ * Data structures for the kconsumerd communications
+ *
+ * The header structure is sent to the kconsumerd daemon to inform
+ * how many lttcomm_kconsumerd_msg it is about to receive
+ */
+struct lttcomm_kconsumerd_header {
+	uint32_t payload_size;
+	uint32_t cmd_type;	/* enum kconsumerd_command */
+};
+
+/* lttcomm_kconsumerd_msg represents a file descriptor to consume the
+ * data and a path name to write it
+ */
+struct lttcomm_kconsumerd_msg {
+	char path_name[PATH_MAX];
+	int fd;
+	uint32_t state;    /* enum lttcomm_kconsumerd_fd_state */
+	unsigned long max_sb_size; /* the subbuffer size for this channel */
+};
+
+extern int lttcomm_create_unix_sock(const char *pathname);
+extern int lttcomm_connect_unix_sock(const char *pathname);
+extern int lttcomm_accept_unix_sock(int sock);
+extern int lttcomm_listen_unix_sock(int sock);
+extern int lttcomm_close_unix_sock(int sock);
+/* Send fd(s) over a unix socket. */
+extern ssize_t lttcomm_send_fds_unix_sock(int sock, void *buf, int *fds,
+		size_t nb_fd, size_t len);
+extern ssize_t lttcomm_recv_unix_sock(int sock, void *buf, size_t len);
+extern ssize_t lttcomm_send_unix_sock(int sock, void *buf, size_t len);
+extern const char *lttcomm_get_readable_code(enum lttcomm_return_code code);
+
+#endif	/* _LTTNG_SESSIOND_COMM_H */
---
+++ liblttng-sessiond-comm/lttng-sessiond-comm.c
@@ -0,0 +1,340 @@
+/*
+ * Copyright (C)  2011 - David Goulet <david.goulet@polymtl.ca>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; only version 2
+ * of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#define _GNU_SOURCE
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <lttng-sessiond-comm.h>
+
+/*
+ * Human readable error message.
+ */
+static const char *lttcomm_readable_code[] = {
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_OK) ] = "Success",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_ERR) ] = "Unknown error",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_UND) ] = "Undefined command",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_NOT_IMPLEMENTED) ] = "Not implemented",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_UNKNOWN_DOMAIN) ] = "Unknown tracing domain",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_NO_SESSION) ] = "No session found",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_LIST_FAIL) ] = "Unable to list traceable apps",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_NO_APPS) ] = "No traceable apps found",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_SESS_NOT_FOUND) ] = "Session name not found",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_NO_TRACE) ] = "No trace found",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_FATAL) ] = "Fatal error of the session daemon",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_CREATE_FAIL) ] = "Create trace failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_START_FAIL) ] = "Start trace failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_STOP_FAIL) ] = "Stop trace failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_NO_TRACEABLE) ] = "App is not traceable",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_SELECT_SESS) ] = "A session MUST be selected",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_EXIST_SESS) ] = "Session name already exist",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_NA) ] = "Kernel tracer not available",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_EVENT_EXIST) ] = "Kernel event already exists",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_SESS_FAIL) ] = "Kernel create session failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_CHAN_FAIL) ] = "Kernel create channel failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_CHAN_NOT_FOUND) ] = "Kernel channel not found",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_CHAN_DISABLE_FAIL) ] = "Disable kernel channel failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_CHAN_ENABLE_FAIL) ] = "Enable kernel channel failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_CONTEXT_FAIL) ] = "Add kernel context failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_ENABLE_FAIL) ] = "Enable kernel event failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_DISABLE_FAIL) ] = "Disable kernel event failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_META_FAIL) ] = "Opening metadata failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_START_FAIL) ] = "Starting kernel trace failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_STOP_FAIL) ] = "Stoping kernel trace failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_CONSUMER_FAIL) ] = "Kernel consumer start failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_STREAM_FAIL) ] = "Kernel create stream failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_DIR_FAIL) ] = "Kernel trace directory creation failed",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_DIR_EXIST) ] = "Kernel trace directory already exist",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_NO_SESSION) ] = "No kernel session found",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_KERN_LIST_FAIL) ] = "Listing kernel events failed",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_COMMAND_SOCK_READY) ] = "Kconsumerd command socket ready",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_SUCCESS_RECV_FD) ] = "Kconsumerd success on receiving fds",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_ERROR_RECV_FD) ] = "Kconsumerd error on receiving fds",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_POLL_ERROR) ] = "Kconsumerd error in polling thread",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_POLL_NVAL) ] = "Kconsumerd polling on closed fd",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_POLL_HUP) ] = "Kconsumerd all fd hung up",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_EXIT_SUCCESS) ] = "Kconsumerd exiting normally",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_EXIT_FAILURE) ] = "Kconsumerd exiting on error",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_OUTFD_ERROR) ] = "Kconsumerd error opening the tracefile",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_SPLICE_EBADF) ] = "Kconsumerd splice EBADF",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_SPLICE_EINVAL) ] = "Kconsumerd splice EINVAL",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_SPLICE_ENOMEM) ] = "Kconsumerd splice ENOMEM",
+	[ LTTCOMM_ERR_INDEX(KCONSUMERD_SPLICE_ESPIPE) ] = "Kconsumerd splice ESPIPE",
+	[ LTTCOMM_ERR_INDEX(LTTCOMM_NO_EVENT) ] = "Event not found",
+};
+
+/*
+ *  lttcom_get_readable_code
+ *
+ *  Return ptr to string representing a human readable
+ *  error code from the lttcomm_return_code enum.
+ *
+ *  These code MUST be negative in other to treat that
+ *  as an error value.
+ */
+const char *lttcomm_get_readable_code(enum lttcomm_return_code code)
+{
+	int tmp_code = -code;
+
+	if (tmp_code >= LTTCOMM_OK && tmp_code < LTTCOMM_NR) {
+		return lttcomm_readable_code[LTTCOMM_ERR_INDEX(tmp_code)];
+	}
+
+	return "Unknown error code";
+}
+
+/*
+ * 	lttcomm_connect_unix_sock
+ *
+ * 	Connect to unix socket using the path name.
+ */
+int lttcomm_connect_unix_sock(const char *pathname)
+{
+	struct sockaddr_un sun;
+	int fd;
+	int ret;
+
+	fd = socket(PF_UNIX, SOCK_STREAM, 0);
+	if (fd < 0) {
+		perror("socket");
+		ret = fd;
+		goto error;
+	}
+
+	memset(&sun, 0, sizeof(sun));
+	sun.sun_family = AF_UNIX;
+	strncpy(sun.sun_path, pathname, sizeof(sun.sun_path));
+	sun.sun_path[sizeof(sun.sun_path) - 1] = '\0';
+
+	ret = connect(fd, (struct sockaddr *) &sun, sizeof(sun));
+	if (ret < 0) {
+		/*
+		 * Don't print message on connect error, because connect
+		 * is used in normal execution to detect if sessiond is
+		 * alive.
+		 */
+		goto error_connect;
+	}
+
+	return fd;
+
+error_connect:
+	close(fd);
+error:
+	return ret;
+}
+
+/*
+ * 	lttcomm_accept_unix_sock
+ *
+ *	Do an accept(2) on the sock and return the
+ *	new file descriptor. The socket MUST be bind(2) before.
+ */
+int lttcomm_accept_unix_sock(int sock)
+{
+	int new_fd;
+	struct sockaddr_un sun;
+	socklen_t len = 0;
+
+	/* Blocking call */
+	new_fd = accept(sock, (struct sockaddr *) &sun, &len);
+	if (new_fd < 0) {
+		perror("accept");
+		goto error;
+	}
+
+	return new_fd;
+
+error:
+	return -1;
+}
+
+/*
+ * 	lttcomm_create_unix_sock
+ *
+ * 	Creates a AF_UNIX local socket using pathname
+ * 	bind the socket upon creation and return the fd.
+ */
+int lttcomm_create_unix_sock(const char *pathname)
+{
+	struct sockaddr_un sun;
+	int fd;
+	int ret = -1;
+
+	/* Create server socket */
+	if ((fd = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		goto error;
+	}
+
+	memset(&sun, 0, sizeof(sun));
+	sun.sun_family = AF_UNIX;
+	strncpy(sun.sun_path, pathname, sizeof(sun.sun_path));
+	sun.sun_path[sizeof(sun.sun_path) - 1] = '\0';
+
+	/* Unlink the old file if present */
+	(void) unlink(pathname);
+	ret = bind(fd, (struct sockaddr *) &sun, sizeof(sun));
+	if (ret < 0) {
+		perror("bind");
+		goto error;
+	}
+
+	return fd;
+
+error:
+	return ret;
+}
+
+/*
+ * 	lttcomm_listen_unix_sock
+ *
+ * 	Make the socket listen using MAX_LISTEN.
+ */
+int lttcomm_listen_unix_sock(int sock)
+{
+	int ret;
+
+	ret = listen(sock, MAX_LISTEN);
+	if (ret < 0) {
+		perror("listen");
+	}
+
+	return ret;
+}
+
+/*
+ * 	lttcomm_recv_unix_sock
+ *
+ *  Receive data of size len in put that data into
+ *  the buf param. Using recvmsg API.
+ *  Return the size of received data.
+ */
+ssize_t lttcomm_recv_unix_sock(int sock, void *buf, size_t len)
+{
+	struct msghdr msg = { 0 };
+	struct iovec iov[1];
+	ssize_t ret = -1;
+
+	iov[0].iov_base = buf;
+	iov[0].iov_len = len;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+	ret = recvmsg(sock, &msg, 0);
+	if (ret < 0) {
+		perror("recvmsg");
+	}
+
+	return ret;
+}
+
+/*
+ * 	lttcomm_send_unix_sock
+ *
+ * 	Send buf data of size len. Using sendmsg API.
+ * 	Return the size of sent data.
+ */
+ssize_t lttcomm_send_unix_sock(int sock, void *buf, size_t len)
+{
+	struct msghdr msg = { 0 };
+	struct iovec iov[1];
+	ssize_t ret = -1;
+
+	iov[0].iov_base = buf;
+	iov[0].iov_len = len;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+	ret = sendmsg(sock, &msg, 0);
+	if (ret < 0) {
+		perror("sendmsg");
+	}
+
+	return ret;
+}
+
+/*
+ *  lttcomm_close_unix_sock
+ *
+ *  Shutdown cleanly a unix socket.
+ */
+int lttcomm_close_unix_sock(int sock)
+{
+	int ret;
+
+	/* Shutdown receptions and transmissions */
+	ret = shutdown(sock, SHUT_RDWR);
+	if (ret < 0) {
+		perror("shutdown");
+	}
+
+	return ret;
+}
+
+/*
+ *  lttcomm_send_fds_unix_sock
+ *
+ *  Send multiple fds on a unix socket.
+ */
+ssize_t lttcomm_send_fds_unix_sock(int sock, void *buf, int *fds, size_t nb_fd, size_t len)
+{
+	struct msghdr msg = { 0 };
+	struct cmsghdr *cmptr;
+	struct iovec iov[1];
+	ssize_t ret = -1;
+	unsigned int sizeof_fds = nb_fd * sizeof(int);
+	char tmp[CMSG_SPACE(sizeof_fds)];
+
+	/*
+	 * Note: the consumerd receiver only supports receiving one FD per
+	 * message.
+	 */
+	assert(nb_fd == 1);
+
+	msg.msg_control = (caddr_t)tmp;
+	msg.msg_controllen = CMSG_LEN(sizeof_fds);
+
+	cmptr = CMSG_FIRSTHDR(&msg);
+	cmptr->cmsg_level = SOL_SOCKET;
+	cmptr->cmsg_type = SCM_RIGHTS;
+	cmptr->cmsg_len = CMSG_LEN(sizeof_fds);
+	memcpy(CMSG_DATA(cmptr), fds, sizeof_fds);
+	/* Sum of the length of all control messages in the buffer: */
+	msg.msg_controllen = cmptr->cmsg_len;
+
+	iov[0].iov_base = buf;
+	iov[0].iov_len = len;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+	ret = sendmsg(sock, &msg, 0);
+	if (ret < 0) {
+		perror("sendmsg");
+	}
+
+	return ret;
+}
