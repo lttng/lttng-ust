d13aca5c82f8f367179b229c40a953fc3275badf
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-08-23 16:48:23 -0400

    Cleanup
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- tests/hello/ust_tests_hello.h
+++ tests/hello/ust_tests_hello.h
@@ -9,8 +9,8 @@
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * License as published by the Free Software Foundation; version 2.1 of
+ * the License.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -51,7 +51,7 @@ TRACEPOINT_EVENT_NOARGS(ust_tests_hello_tptest_sighandler,
 			TP_FIELDS()
 		)
 
-#endif /* _TRACEPOINT_TP_H */
+#endif /* _TRACEPOINT_UST_TESTS_HELLO_H */
 
 #undef TRACEPOINT_INCLUDE_PATH
 #define TRACEPOINT_INCLUDE_PATH .
