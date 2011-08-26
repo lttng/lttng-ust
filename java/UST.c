2b6f8df951cb74149a9ab9c5146e6dd8498b9e87
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-08-26 07:23:16 -0400

    Add TRACEPOINT_EVENT java instrumentation wrapper
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- java/UST.c
+++ java/UST.c
@@ -1,13 +1,17 @@
 #include <jni.h>
-#include <ust/marker.h>
 
-JNIEXPORT void JNICALL Java_UST_ust_1java_1event (JNIEnv *env, jobject jobj, jstring ev_name, jstring args)
+#define TRACEPOINT_CREATE_PROBES
+#include "ust_java.h"
+
+JNIEXPORT void JNICALL Java_UST_ust_1java_1event (JNIEnv *env, jobject jobj,
+						jstring ev_name, jstring args)
 {
 	jboolean iscopy;
-	const char *ev_name_cstr = (*env)->GetStringUTFChars(env, ev_name, &iscopy);
+	const char *ev_name_cstr = (*env)->GetStringUTFChars(env, ev_name,
+							&iscopy);
 	const char *args_cstr = (*env)->GetStringUTFChars(env, args, &iscopy);
 
-	ust_marker(ust, java_event, "name %s args %s", ev_name_cstr, args_cstr);
+	tracepoint(ust_java_event, ev_name_cstr, args_cstr);
 }
 
-UST_MARKER_LIB
+TRACEPOINT_LIB
---
+++ java/ust_java.h
@@ -0,0 +1,44 @@
+#undef TRACEPOINT_SYSTEM
+#define TRACEPOINT_SYSTEM ust_java
+
+#if !defined(_TRACEPOINT_UST_JAVA_H) || defined(TRACEPOINT_HEADER_MULTI_READ)
+#define _TRACEPOINT_UST_JAVA_H
+
+/*
+ * Copyright (C) 2011  Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <ust/tracepoint.h>
+
+TRACEPOINT_EVENT(ust_java_event,
+	TP_PROTO(const char *name, const char *args),
+	TP_ARGS(name, args),
+	TP_FIELDS(
+		ctf_string(name, name)
+		ctf_string(args, args)
+	)
+)
+
+#endif /* _TRACEPOINT_UST_JAVA_H */
+
+#undef TRACEPOINT_INCLUDE_PATH
+#define TRACEPOINT_INCLUDE_PATH .
+#undef TRACEPOINT_INCLUDE_FILE
+#define TRACEPOINT_INCLUDE_FILE ust_java
+
+/* This part must be outside protection */
+#include <ust/tracepoint-event.h>
