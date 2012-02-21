7705281be801c16b35349f9ab17f67419cac1c34
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-02-20 21:54:04 -0500

    Use XSI-compliant strerror_r by default on non-Linux
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/usterr.h
+++ include/usterr.h
@@ -87,7 +87,10 @@ static inline int ust_debug(void)
 #define ERR(fmt, args...) ERRMSG("Error: " fmt, ## args)
 #define BUG(fmt, args...) ERRMSG("BUG: " fmt, ## args)
 
-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !defined(_GNU_SOURCE)
+#if !defined(__linux__) || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !defined(_GNU_SOURCE))
+/*
+ * Version using XSI strerror_r.
+ */
 #define PERROR(call, args...)\
 	do { \
 		char buf[200] = "Error in strerror_r()"; \
@@ -95,6 +98,9 @@ static inline int ust_debug(void)
 		ERRMSG("Error: " call ": %s", ## args, buf); \
 	} while(0);
 #else
+/*
+ * Version using GNU strerror_r, for linux with appropriate defines.
+ */
 #define PERROR(call, args...)\
 	do { \
 		char *buf; \
