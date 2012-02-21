75181c4b1b6e046ae8561c486ced003a83b1a696
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-02-20 19:28:44 -0500

    Fix freebsd endian support
    Need to use #elif defined().
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/lttng/ust-endian.h
+++ include/lttng/ust-endian.h
@@ -20,7 +20,7 @@
 
 #ifdef __linux__
 #include <endian.h>
-#elif __FreeBSD__
+#elif defined(__FreeBSD__)
 #include <machine/endian.h>
 #else
 #error "Please add support for your OS into lttng/ust-endian.h."
