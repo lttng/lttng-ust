e52b0723a0f08966ee0913707b549235b2bce96d
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-02-21 19:20:51 -0500

    Fix: Return -EINVAL instead of print warning if non power of 2 size/num_subbuf
    Reported-by: Tan Dung Le Tran <tan.dung.le.tran@ericsson.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- libringbuffer/ring_buffer_backend.c
+++ libringbuffer/ring_buffer_backend.c
@@ -220,8 +220,10 @@ int channel_backend_init(struct channel_backend *chanb,
 	/*
 	 * Make sure the number of subbuffers and subbuffer size are power of 2.
 	 */
-	CHAN_WARN_ON(chanb, hweight32(subbuf_size) != 1);
-	CHAN_WARN_ON(chanb, hweight32(num_subbuf) != 1);
+	if (hweight32(subbuf_size) != 1)
+		return -EINVAL;
+	if (hweight32(num_subbuf) != 1)
+		return -EINVAL;
 
 	ret = subbuffer_id_check_index(config, num_subbuf);
 	if (ret)
