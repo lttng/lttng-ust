bd68f05335c6f9831d1da9403b0854a7df16d2ca
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-04-12 14:06:14 -0400

    Revert "Build a jar for the Java side of the JNI interface"
    This reverts commit 6dcf3689333a2b465a5738c840d0b14d3f8014ea.
    
    This should not go into stable-2.0.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- liblttng-ust-java/LTTngUst.java
+++ liblttng-ust-java/LTTngUst.java
@@ -18,7 +18,7 @@
 
 package org.lttng.ust;
 
-public class LTTngUst {
+class LTTngUst {
 	public static native void tracepointString(String name, String arg);
 	static {
 		System.loadLibrary("lttng-ust-java");
