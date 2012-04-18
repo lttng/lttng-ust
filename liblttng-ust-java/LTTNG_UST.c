8bc39f8703eb1189eb9d8b91345a554ccfcb2975
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-04-18 19:05:32 -0400

    Revert "Fix: Make the JNI interface actually work"
    This reverts commit 46adf653a1d01b8e87baa30cc9b92616eee1a788.
    
    It changes the makefiles, and broke make dist. This does not belong in a
    stable release.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- liblttng-ust-java/LTTNG_UST.c
+++ liblttng-ust-java/LTTNG_UST.c
@@ -16,12 +16,21 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-package org.lttng.ust;
+#include <jni.h>
 
-class LTTngUst {
-	public static native void tracepointString(String name, String arg);
-	static {
-		System.loadLibrary("lttng-ust-java");
-	}
+#define TRACEPOINT_DEFINE
+#define TRACEPOINT_CREATE_PROBES
+#include "lttng_ust_java.h"
+
+JNIEXPORT void JNICALL Java_LTTNG_UST_ust_1java_1event (JNIEnv *env,
+						jobject jobj,
+						jstring ev_name,
+						jstring args)
+{
+	jboolean iscopy;
+	const char *ev_name_cstr = (*env)->GetStringUTFChars(env, ev_name,
+							&iscopy);
+	const char *args_cstr = (*env)->GetStringUTFChars(env, args, &iscopy);
+
+	tracepoint(lttng_ust_java, string, ev_name_cstr, args_cstr);
 }
-
