2e349edd406c641a9437020451b718e5bc9253e0
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-06-11 10:15:25 -0400

    Fix c99 compatibility: use __typeof__ instead of typeof in public headers
    Reported-by: John Steele Scott <toojays@toojays.net>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/lttng/align.h
+++ include/lttng/align.h
@@ -23,7 +23,7 @@
 #define PAGE_SIZE		sysconf(_SC_PAGE_SIZE)
 #define PAGE_MASK		(~(PAGE_SIZE - 1))
 #define __ALIGN_MASK(v, mask)	(((v) + (mask)) & ~(mask))
-#define ALIGN(v, align)		__ALIGN_MASK(v, (typeof(v)) (align) - 1)
+#define ALIGN(v, align)		__ALIGN_MASK(v, (__typeof__(v)) (align) - 1)
 #define PAGE_ALIGN(addr)	ALIGN(addr, PAGE_SIZE)
 
 /**
