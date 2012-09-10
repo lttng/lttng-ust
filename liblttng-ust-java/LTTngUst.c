ca7bd68c5510cffbbb282d53eff2332d31429795
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-09-10 17:09:20 -0400

    Fix make dist: fix liblttng-ust-java dependencies
    Acked-by: Alexandre Montplaisir <alexandre.montplaisir@polymtl.ca>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- liblttng-ust-java/LTTngUst.c
+++ liblttng-ust-java/LTTngUst.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <jni.h>
+#include "org_lttng_ust_LTTngUst.h"
 
 #define TRACEPOINT_DEFINE
 #define TRACEPOINT_CREATE_PROBES
