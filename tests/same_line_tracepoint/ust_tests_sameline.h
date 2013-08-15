177d5e84ca824cd51c41cc380b0f7600e27e39a6
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2013-08-15 10:55:30 -0400

    Tracepoint probes don't need extern C
    tracepoint.h has the proper extern C guards, so every liblttng-ust API
    called from the probe has the proper linkage.
    
    The rest of the symbols are only meaningful within the probe, so we
    don't care if they are mangled or not.
    
    This fixes the two examples, and lttng-gen-tp.
    
    Fixes #597
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- tests/same_line_tracepoint/ust_tests_sameline.h
+++ tests/same_line_tracepoint/ust_tests_sameline.h
@@ -4,10 +4,6 @@
 #if !defined(_TRACEPOINT_UST_TESTS_SAMELINE_H) || defined(TRACEPOINT_HEADER_MULTI_READ)
 #define _TRACEPOINT_UST_TESTS_SAMELINE_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /*
  * Copyright (C) 2011  Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
  *
@@ -51,7 +47,3 @@ TRACEPOINT_LOGLEVEL(ust_tests_sameline, event2, TRACE_CRIT)
 
 /* This part must be outside ifdef protection */
 #include <lttng/tracepoint-event.h>
-
-#ifdef __cplusplus
-}
-#endif
