866f2767f671ffbbff2c0511964f5a9a66358184
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-05-13 09:34:03 -0400

    Fix: tracef.h: add missing c++ guard
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/lttng/tracef.h
+++ include/lttng/tracef.h
@@ -25,6 +25,10 @@
 
 #include <lttng/lttng-ust-tracef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern
 void _lttng_ust_tracef(const char *fmt, ...);
 
@@ -34,5 +38,9 @@ void _lttng_ust_tracef(const char *fmt, ...);
 		if (caa_unlikely(__tracepoint_lttng_ust_tracef___event.state)) \
 			_lttng_ust_tracef(fmt, ## __VA_ARGS__);		\
 	} while (0)
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _LTTNG_UST_TRACEF_H */
