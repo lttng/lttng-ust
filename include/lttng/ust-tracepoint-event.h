4f6c72c9068d6a6475da84c08f0919dc1207e8c1
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-11-12 16:05:36 -0500

    Fix coverity warning about sizeof(void **) != sizeof(void *)
    The size of the copied data is actually void *, not void **.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/lttng/ust-tracepoint-event.h
+++ include/lttng/ust-tracepoint-event.h
@@ -385,8 +385,8 @@ size_t __event_get_size__##_provider##___##_name(size_t *__dynamic_len, _TP_ARGS
 		const void *__ctf_tmp_ptr = (_src);			       \
 		memcpy(__stack_data, &__ctf_tmp_ulong, sizeof(unsigned long)); \
 		__stack_data += sizeof(unsigned long);			       \
-		memcpy(__stack_data, &__ctf_tmp_ptr, sizeof(void **));	       \
-		__stack_data += sizeof(void **);			       \
+		memcpy(__stack_data, &__ctf_tmp_ptr, sizeof(void *));	       \
+		__stack_data += sizeof(void *);				       \
 	}
 
 #undef _ctf_sequence_encoded
@@ -397,16 +397,16 @@ size_t __event_get_size__##_provider##___##_name(size_t *__dynamic_len, _TP_ARGS
 		const void *__ctf_tmp_ptr = (_src);			       \
 		memcpy(__stack_data, &__ctf_tmp_ulong, sizeof(unsigned long)); \
 		__stack_data += sizeof(unsigned long);			       \
-		memcpy(__stack_data, &__ctf_tmp_ptr, sizeof(void **));	       \
-		__stack_data += sizeof(void **);			       \
+		memcpy(__stack_data, &__ctf_tmp_ptr, sizeof(void *));	       \
+		__stack_data += sizeof(void *);				       \
 	}
 
 #undef _ctf_string
 #define _ctf_string(_item, _src, _nowrite)				       \
 	{								       \
 		const void *__ctf_tmp_ptr = (_src);			       \
-		memcpy(__stack_data, &__ctf_tmp_ptr, sizeof(void **));	       \
-		__stack_data += sizeof(void **);			       \
+		memcpy(__stack_data, &__ctf_tmp_ptr, sizeof(void *));	       \
+		__stack_data += sizeof(void *);				       \
 	}
 
 #undef TP_ARGS
