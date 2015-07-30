51761fb97d8382b89632310967a9ec87536225d4
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2015-07-30 16:43:21 -0400

    Rename Python agent tracepoint to "event"
    This rename comes as an effort to remain consistent with the tracepoint
    naming scheme used by Log4J and JUL.
    
    The associated change in lttng-tools is introduced as of d8c7bda0.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- liblttng-ust-python-agent/lttng_ust_python.c
+++ liblttng-ust-python-agent/lttng_ust_python.c
@@ -27,6 +27,6 @@ void py_tracepoint(const char *asctime, const char *msg,
 		const char *logger_name, const char *funcName, unsigned int lineno,
 		unsigned int int_loglevel, unsigned int thread, const char *threadName)
 {
-	tracepoint(lttng_python, user_event, asctime, msg, logger_name, funcName,
+	tracepoint(lttng_python, event, asctime, msg, logger_name, funcName,
 			lineno, int_loglevel, thread, threadName);
 }
