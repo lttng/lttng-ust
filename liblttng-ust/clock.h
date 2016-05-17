776860373a25f9dcbe68e2690a34d7c641e7ef00
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-05-17 12:05:10 -0400

    Fix: unchecked return value in trace_clock_read64_monotonic
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- liblttng-ust/clock.h
+++ liblttng-ust/clock.h
@@ -50,7 +50,10 @@ uint64_t trace_clock_read64_monotonic(void)
 {
 	struct timespec ts;
 
-	clock_gettime(CLOCK_MONOTONIC, &ts);
+	if (caa_unlikely(clock_gettime(CLOCK_MONOTONIC, &ts))) {
+		ts.tv_sec = 0;
+		ts.tv_nsec = 0;
+	}
 	return ((uint64_t) ts.tv_sec * 1000000000ULL) + ts.tv_nsec;
 }
 
