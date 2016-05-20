18d24538d964e8d3b86df9fa68d44ba136a4d8c0
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-05-20 15:23:06 -0400

    Print DBG() message about compiler weak hidden symbol behavior
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ liblttng-ust/tracepoint-weak-test.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2016 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+/* Test compiler support for weak symbols with hidden visibility. */
+char __tracepoint_test_symbol[9] __attribute__((weak, visibility("hidden")));
+
+__attribute__((visibility("hidden")))
+void *lttng_ust_tp_check_weak_hidden(void)
+{
+	return __tracepoint_test_symbol;
+}
