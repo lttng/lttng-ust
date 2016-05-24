1de7abffafd64c964b738926f1072f5704e8e51f
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-05-24 18:58:14 -0400

    Fix: don't call __builtin_return_address(0) on 32-bit powerpc
    Invoking __builtin_return_address(0) corrupts the stack, as previously
    noticed for the "ip" context. Disable its use on 32-bit powerpc
    everywhere else in the lttng-ust code base.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/helper.h
+++ include/helper.h
@@ -43,4 +43,16 @@ void *zmalloc(size_t len)
 
 #define LTTNG_ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
+/*
+ * Use of __builtin_return_address(0) sometimes seems to cause stack
+ * corruption on 32-bit PowerPC. Disable this feature on that
+ * architecture for now by always using the NULL value for the ip
+ * context.
+ */
+#if defined(__PPC__) && !defined(__PPC64__)
+#define LTTNG_UST_CALLER_IP()		NULL
+#else /* #if defined(__PPC__) && !defined(__PPC64__) */
+#define LTTNG_UST_CALLER_IP()		__builtin_return_address(0)
+#endif /* #else #if defined(__PPC__) && !defined(__PPC64__) */
+
 #endif /* _LTTNG_UST_HELPER_H */
