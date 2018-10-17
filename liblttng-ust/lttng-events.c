3ff7660f2ce6362de87a48bc45c9a98b9c785472
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2018-10-17 15:48:16 -0400

    Fix: sync event enablers before choosing header type
    On session start, we should allocate the event IDs before figuring
    out the number of events per channel and select the proper header
    type.
    
    Without this, the number of events is always perceived to be 0,
    which selects the "compact" header type.
    
    With UST, the order of those two actions is not the only one
    affecting the chosen header type: UST can receive the session
    start command before all probe provider library constructors
    have completed running, therefore finding less events than
    eventually enabled within the process. Moreover, with per-uid
    buffers, many processes end up registering events into shared
    buffers. Therefore, the guess based on number of events from
    the first process to use the buffer is incorrect.
    
    Considering that we typically have applications with more than
    30 events, we will modify the session daemon so it selects the
    "large" header type independently of the number of events.
    
    We still want to swap the order of the enablers sync vs header
    type getter because we may revisit how session enabling is done
    in a process wrt constructor completion, which may allow us to
    do a more precise choice for per-pid buffers in the future.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- liblttng-ust/lttng-events.c
+++ liblttng-ust/lttng-events.c
@@ -415,6 +415,9 @@ int lttng_session_enable(struct lttng_session *session)
 	/* Set transient enabler state to "enabled" */
 	session->tstate = 1;
 
+	/* We need to sync enablers with session before activation. */
+	lttng_session_sync_enablers(session);
+
 	/*
 	 * Snapshot the number of events per channel to know the type of header
 	 * we need to use.
@@ -457,9 +460,6 @@ int lttng_session_enable(struct lttng_session *session)
 			return -EINVAL;
 		}
 	}
-
-	/* We need to sync enablers with session before activation. */
-	lttng_session_sync_enablers(session);
 
 	/* Set atomically the state to "active" */
 	CMM_ACCESS_ONCE(session->active) = 1;
