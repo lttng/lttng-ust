lttng/lttng-ust

commit 218deb69baab57ee2f6728eef18e84697f21197b

tracepoint: Refactor representation of nested types
Refactor enumeration, sequence, array, structure, and variant types.

Implement internal data structures to support nested types.

All probe providers using ctf_enum(), ctf_array*() and ctf_sequence*()
are switched to this new binary layout, and the prior enum, array and
sequence abstract types are kept only for backward compatibility with
probe providers generated by older lttng-ust headers.

Each of sequence, array, struct and variant gain a "alignment" property,
which is a feature which was needed in lttng-modules to express
alignment for an array or sequence of bits.

The ust-ctl protocol with session daemon is extended to support those
new types.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

liblttng-ust-comm/lttng-ust-comm.c
@@ -55,6 +55,11 @@ static
 int serialize_one_field(struct lttng_session *session,
 		struct ustctl_field *fields, size_t *iter_output,
 		const struct lttng_event_field *lf);
+static
+int serialize_fields(struct lttng_session *session,
+		struct ustctl_field *ustctl_fields,
+		size_t *iter_output, size_t nr_lttng_fields,
+		const struct lttng_event_field *lttng_fields);
 
 /*
  * Human readable error message.
@@ -773,8 +778,18 @@ ssize_t count_one_type(const struct lttng_type *lt)
 	case atype_sequence:
 		return 1;
 	case atype_struct:
-		//TODO: implement non-empty struct.
-		return 1;
+		return count_fields_recursive(lt->u.legacy._struct.nr_fields,
+				lt->u.legacy._struct.fields);
+	case atype_enum_nestable:
+		return count_one_type(lt->u.enum_nestable.container_type) + 1;
+	case atype_array_nestable:
+		return count_one_type(lt->u.array_nestable.elem_type) + 1;
+	case atype_sequence_nestable:
+		return count_one_type(lt->u.sequence_nestable.elem_type) + 1;
+	case atype_struct_nestable:
+		return count_fields_recursive(lt->u.struct_nestable.nr_fields,
+				lt->u.struct_nestable.fields);
+
 	case atype_dynamic:
 	{
 		const struct lttng_event_field *choices;
@@ -791,6 +806,7 @@ ssize_t count_one_type(const struct lttng_type *lt)
 		 */
 		return count_fields_recursive(nr_choices, choices) + 2;
 	}
+
 	default:
 		return -EINVAL;
 	}
@@ -939,7 +955,10 @@ int serialize_basic_type(struct lttng_session *session,
 		break;
 	}
 	case atype_array:
+	case atype_array_nestable:
 	case atype_sequence:
+	case atype_sequence_nestable:
+	case atype_enum_nestable:
 	default:
 		return -EINVAL;
 	}
@@ -949,7 +968,7 @@ int serialize_basic_type(struct lttng_session *session,
 static
 int serialize_dynamic_type(struct lttng_session *session,
 		struct ustctl_field *fields, size_t *iter_output,
-		const struct lttng_event_field *lf)
+		const char *field_name)
 {
 	const struct lttng_event_field *choices;
 	char tag_field_name[LTTNG_UST_SYM_NAME_LEN];
@@ -967,7 +986,7 @@ int serialize_dynamic_type(struct lttng_session *session,
 	tag_type = &tag_field_generic->type;
 
 	/* Serialize enum field. */
-	strncpy(tag_field_name, lf->name, LTTNG_UST_SYM_NAME_LEN);
+	strncpy(tag_field_name, field_name, LTTNG_UST_SYM_NAME_LEN);
 	tag_field_name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
 	strncat(tag_field_name,
 		"_tag",
@@ -984,14 +1003,15 @@ int serialize_dynamic_type(struct lttng_session *session,
 	if (ret)
 		return ret;
 
-	strncpy(uf->name, lf->name, LTTNG_UST_SYM_NAME_LEN);
+	strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
 	uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
 	uf->type.atype = ustctl_atype_variant;
-	uf->type.u.variant.nr_choices = nr_choices;
-	strncpy(uf->type.u.variant.tag_name,
+	uf->type.u.variant_nestable.nr_choices = nr_choices;
+	strncpy(uf->type.u.variant_nestable.tag_name,
 		tag_field_name,
 		LTTNG_UST_SYM_NAME_LEN);
-	uf->type.u.variant.tag_name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+	uf->type.u.variant_nestable.tag_name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+	uf->type.u.variant_nestable.alignment = 0;
 	(*iter_output)++;
 
 	/* Serialize choice fields after variant. */
@@ -1005,16 +1025,16 @@ int serialize_dynamic_type(struct lttng_session *session,
 }
 
 static
-int serialize_one_field(struct lttng_session *session,
+int serialize_one_type(struct lttng_session *session,
 		struct ustctl_field *fields, size_t *iter_output,
-		const struct lttng_event_field *lf)
+		const char *field_name, const struct lttng_type *lt)
 {
-	const struct lttng_type *lt = &lf->type;
 	int ret;
 
-	/* skip 'nowrite' fields */
-	if (lf->nowrite)
-		return 0;
+	/*
+	 * Serializing a type (rather than a field) generates a ustctl_field
+	 * entry with 0-length name.
+	 */
 
 	switch (lt->atype) {
 	case atype_integer:
@@ -1026,10 +1046,14 @@ int serialize_one_field(struct lttng_session *session,
 		struct ustctl_type *ut = &uf->type;
 		enum ustctl_abstract_types atype;
 
-		strncpy(uf->name, lf->name, LTTNG_UST_SYM_NAME_LEN);
-		uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
+		}
 		ret = serialize_basic_type(session, &atype, lt->atype,
-			&ut->u.basic, &lt->u.basic);
+			&ut->u.legacy.basic, &lt->u.legacy.basic);
 		if (ret)
 			return ret;
 		ut->atype = atype;
@@ -1044,19 +1068,44 @@ int serialize_one_field(struct lttng_session *session,
 		const struct lttng_basic_type *lbt;
 		enum ustctl_abstract_types atype;
 
-		strncpy(uf->name, lf->name, LTTNG_UST_SYM_NAME_LEN);
-		uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
-		uf->type.atype = ustctl_atype_array;
-		ubt = &ut->u.array.elem_type;
-		lbt = &lt->u.array.elem_type;
-		ut->u.array.length = lt->u.array.length;
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
+		}
+		ut->atype = ustctl_atype_array;
+		ubt = &ut->u.legacy.array.elem_type;
+		lbt = &lt->u.legacy.array.elem_type;
+		ut->u.legacy.array.length = lt->u.legacy.array.length;
 		ret = serialize_basic_type(session, &atype, lbt->atype,
 			&ubt->u.basic, &lbt->u.basic);
 		if (ret)
 			return -EINVAL;
 		ubt->atype = atype;
-		ut->atype = ustctl_atype_array;
 		(*iter_output)++;
+		break;
+	}
+	case atype_array_nestable:
+	{
+		struct ustctl_field *uf = &fields[*iter_output];
+		struct ustctl_type *ut = &uf->type;
+
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
+		}
+		ut->atype = ustctl_atype_array_nestable;
+		ut->u.array_nestable.length = lt->u.array_nestable.length;
+		ut->u.array_nestable.alignment = lt->u.array_nestable.alignment;
+		(*iter_output)++;
+
+		ret = serialize_one_type(session, fields, iter_output, NULL,
+				lt->u.array_nestable.elem_type);
+		if (ret)
+			return -EINVAL;
 		break;
 	}
 	case atype_sequence:
@@ -1066,32 +1115,60 @@ int serialize_one_field(struct lttng_session *session,
 		struct ustctl_basic_type *ubt;
 		const struct lttng_basic_type *lbt;
 		enum ustctl_abstract_types atype;
-		int ret;
 
-		strncpy(uf->name, lf->name, LTTNG_UST_SYM_NAME_LEN);
-		uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
+		}
 		uf->type.atype = ustctl_atype_sequence;
-		ubt = &ut->u.sequence.length_type;
-		lbt = &lt->u.sequence.length_type;
+		ubt = &ut->u.legacy.sequence.length_type;
+		lbt = &lt->u.legacy.sequence.length_type;
 		ret = serialize_basic_type(session, &atype, lbt->atype,
 			&ubt->u.basic, &lbt->u.basic);
 		if (ret)
 			return -EINVAL;
 		ubt->atype = atype;
-		ubt = &ut->u.sequence.elem_type;
-		lbt = &lt->u.sequence.elem_type;
+		ubt = &ut->u.legacy.sequence.elem_type;
+		lbt = &lt->u.legacy.sequence.elem_type;
 		ret = serialize_basic_type(session, &atype, lbt->atype,
 			&ubt->u.basic, &lbt->u.basic);
 		if (ret)
 			return -EINVAL;
 		ubt->atype = atype;
-		ut->atype = ustctl_atype_sequence;
 		(*iter_output)++;
+		break;
+	}
+	case atype_sequence_nestable:
+	{
+		struct ustctl_field *uf = &fields[*iter_output];
+		struct ustctl_type *ut = &uf->type;
+
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
+		}
+		ut->atype = ustctl_atype_sequence_nestable;
+		strncpy(ut->u.sequence_nestable.length_name,
+			lt->u.sequence_nestable.length_name,
+			LTTNG_UST_SYM_NAME_LEN);
+		ut->u.sequence_nestable.length_name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		ut->u.sequence_nestable.alignment = lt->u.sequence_nestable.alignment;
+		(*iter_output)++;
+
+		ret = serialize_one_type(session, fields, iter_output, NULL,
+				lt->u.sequence_nestable.elem_type);
+		if (ret)
+			return -EINVAL;
 		break;
 	}
 	case atype_dynamic:
 	{
-		ret = serialize_dynamic_type(session, fields, iter_output, lf);
+		ret = serialize_dynamic_type(session, fields, iter_output,
+				field_name);
 		if (ret)
 			return -EINVAL;
 		break;
@@ -1100,17 +1177,76 @@ int serialize_one_field(struct lttng_session *session,
 	{
 		struct ustctl_field *uf = &fields[*iter_output];
 
-		/*
-		 * TODO: add support for non-empty struct.
-		 */
-		if (lf->type.u._struct.nr_fields != 0) {
-			return -EINVAL;
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
 		}
-		strncpy(uf->name, lf->name, LTTNG_UST_SYM_NAME_LEN);
-		uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
 		uf->type.atype = ustctl_atype_struct;
-		uf->type.u._struct.nr_fields = 0;
+		uf->type.u.legacy._struct.nr_fields = lt->u.legacy._struct.nr_fields;
 		(*iter_output)++;
+
+		ret = serialize_fields(session, fields, iter_output,
+				lt->u.legacy._struct.nr_fields,
+				lt->u.legacy._struct.fields);
+		if (ret)
+			return -EINVAL;
+		break;
+	}
+	case atype_struct_nestable:
+	{
+		struct ustctl_field *uf = &fields[*iter_output];
+
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
+		}
+		uf->type.atype = ustctl_atype_struct_nestable;
+		uf->type.u.struct_nestable.nr_fields = lt->u.struct_nestable.nr_fields;
+		uf->type.u.struct_nestable.alignment = lt->u.struct_nestable.alignment;
+		(*iter_output)++;
+
+		ret = serialize_fields(session, fields, iter_output,
+				lt->u.struct_nestable.nr_fields,
+				lt->u.struct_nestable.fields);
+		if (ret)
+			return -EINVAL;
+		break;
+	}
+	case atype_enum_nestable:
+	{
+		struct ustctl_field *uf = &fields[*iter_output];
+		struct ustctl_type *ut = &uf->type;
+
+		if (field_name) {
+			strncpy(uf->name, field_name, LTTNG_UST_SYM_NAME_LEN);
+			uf->name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		} else {
+			uf->name[0] = '\0';
+		}
+		strncpy(ut->u.enum_nestable.name, lt->u.enum_nestable.desc->name,
+				LTTNG_UST_SYM_NAME_LEN);
+		ut->u.enum_nestable.name[LTTNG_UST_SYM_NAME_LEN - 1] = '\0';
+		ut->atype = ustctl_atype_enum_nestable;
+		(*iter_output)++;
+
+		ret = serialize_one_type(session, fields, iter_output, NULL,
+				lt->u.enum_nestable.container_type);
+		if (ret)
+			return -EINVAL;
+		if (session) {
+			const struct lttng_enum *_enum;
+
+			_enum = lttng_ust_enum_get_from_desc(session, lt->u.enum_nestable.desc);
+			if (!_enum)
+				return -EINVAL;
+			ut->u.enum_nestable.id = _enum->id;
+		} else {
+			ut->u.enum_nestable.id = -1ULL;
+		}
 		break;
 	}
 	default:
@@ -1120,7 +1256,37 @@ int serialize_one_field(struct lttng_session *session,
 }
 
 static
+int serialize_one_field(struct lttng_session *session,
+		struct ustctl_field *fields, size_t *iter_output,
+		const struct lttng_event_field *lf)
+{
+	/* skip 'nowrite' fields */
+	if (lf->nowrite)
+		return 0;
+
+	return serialize_one_type(session, fields, iter_output, lf->name, &lf->type);
+}
+
+static
 int serialize_fields(struct lttng_session *session,
+		struct ustctl_field *ustctl_fields,
+		size_t *iter_output, size_t nr_lttng_fields,
+		const struct lttng_event_field *lttng_fields)
+{
+	int ret;
+	size_t i;
+
+	for (i = 0; i < nr_lttng_fields; i++) {
+		ret = serialize_one_field(session, ustctl_fields,
+				iter_output, &lttng_fields[i]);
+		if (ret)
+			return ret;
+	}
+	return 0;
+}
+
+static
+int alloc_serialize_fields(struct lttng_session *session,
 		size_t *_nr_write_fields,
 		struct ustctl_field **ustctl_fields,
 		size_t nr_fields,
@@ -1128,7 +1294,7 @@ int serialize_fields(struct lttng_session *session,
 {
 	struct ustctl_field *fields;
 	int ret;
-	size_t i, iter_output = 0;
+	size_t iter_output = 0;
 	ssize_t nr_write_fields;
 
 	nr_write_fields = count_fields_recursive(nr_fields, lttng_fields);
@@ -1140,12 +1306,10 @@ int serialize_fields(struct lttng_session *session,
 	if (!fields)
 		return -ENOMEM;
 
-	for (i = 0; i < nr_fields; i++) {
-		ret = serialize_one_field(session, fields, &iter_output,
-				&lttng_fields[i]);
-		if (ret)
-			goto error_type;
-	}
+	ret = serialize_fields(session, fields, &iter_output, nr_fields,
+			lttng_fields);
+	if (ret)
+		goto error_type;
 
 	*_nr_write_fields = nr_write_fields;
 	*ustctl_fields = fields;
@@ -1270,7 +1434,7 @@ int ustcomm_register_event(int sock,
 
 	/* Calculate fields len, serialize fields. */
 	if (nr_fields > 0) {
-		ret = serialize_fields(session, &nr_write_fields, &fields,
+		ret = alloc_serialize_fields(session, &nr_write_fields, &fields,
 				nr_fields, lttng_fields);
 		if (ret)
 			return ret;

include/lttng/ust-ctl.h
@@ -313,13 +313,18 @@ enum ustctl_channel_header {
 
 enum ustctl_abstract_types {
 	ustctl_atype_integer,
-	ustctl_atype_enum,
-	ustctl_atype_array,
-	ustctl_atype_sequence,
+	ustctl_atype_enum,	/* legacy */
+	ustctl_atype_array,	/* legacy */
+	ustctl_atype_sequence,	/* legacy */
 	ustctl_atype_string,
 	ustctl_atype_float,
-	ustctl_atype_variant,
-	ustctl_atype_struct,
+	ustctl_atype_variant,	/* legacy */
+	ustctl_atype_struct,	/* legacy */
+	ustctl_atype_enum_nestable,
+	ustctl_atype_array_nestable,
+	ustctl_atype_sequence_nestable,
+	ustctl_atype_struct_nestable,
+	ustctl_atype_variant_nestable,
 	NR_USTCTL_ABSTRACT_TYPES,
 };
 
@@ -373,6 +378,7 @@ struct ustctl_enum_entry {
 	} u;
 } LTTNG_PACKED;
 
+/* legacy */
 #define USTCTL_UST_BASIC_TYPE_PADDING	296
 union _ustctl_basic_type {
 	struct ustctl_integer_type integer;
@@ -388,6 +394,7 @@ union _ustctl_basic_type {
 	char padding[USTCTL_UST_BASIC_TYPE_PADDING];
 } LTTNG_PACKED;
 
+/* legacy */
 struct ustctl_basic_type {
 	enum ustctl_abstract_types atype;
 	union {
@@ -395,28 +402,67 @@ struct ustctl_basic_type {
 	} u;
 } LTTNG_PACKED;
 
-#define USTCTL_UST_TYPE_PADDING	128
+/*
+ * Padding is derived from largest member: u.legacy.sequence which
+ * contains two basic types, each with USTCTL_UST_BASIC_TYPE_PADDING.
+ */
+#define USTCTL_UST_TYPE_PADDING	(2 * USTCTL_UST_BASIC_TYPE_PADDING)
 struct ustctl_type {
 	enum ustctl_abstract_types atype;
 	union {
-		union _ustctl_basic_type basic;
+		struct ustctl_integer_type integer;
+		struct ustctl_float_type _float;
 		struct {
-			struct ustctl_basic_type elem_type;
+			int32_t encoding;	/* enum ustctl_string_encodings */
+		} string;
+		struct {
+			char name[LTTNG_UST_SYM_NAME_LEN];
+			uint64_t id;	/* enum ID in sessiond. */
+			/* container_type follows after this struct ustctl_field. */
+		} enum_nestable;
+		struct {
 			uint32_t length;		/* num. elems. */
-		} array;
+			uint32_t alignment;
+			/* elem_type follows after this struct ustctl_field. */
+		} array_nestable;
 		struct {
-			struct ustctl_basic_type length_type;
-			struct ustctl_basic_type elem_type;
-		} sequence;
+			char length_name[LTTNG_UST_SYM_NAME_LEN];
+			uint32_t alignment;		/* Alignment before elements. */
+			/* elem_type follows after the length_type. */
+		} sequence_nestable;
+		struct {
+			uint32_t nr_fields;
+			uint32_t alignment;
+			/* Followed by nr_fields struct ustctl_field. */
+		} struct_nestable;
 		struct {
 			uint32_t nr_choices;
 			char tag_name[LTTNG_UST_SYM_NAME_LEN];
+			uint32_t alignment;
 			/* Followed by nr_choices struct ustctl_field. */
-		} variant;
-		struct {
-			uint32_t nr_fields;
-			/* Followed by nr_fields struct ustctl_field. */
-		} _struct;
+		} variant_nestable;
+
+		/* Legacy ABI */
+		union {
+			union _ustctl_basic_type basic;
+			struct {
+				struct ustctl_basic_type elem_type;
+				uint32_t length;		/* num. elems. */
+			} array;
+			struct {
+				struct ustctl_basic_type length_type;
+				struct ustctl_basic_type elem_type;
+			} sequence;
+			struct {
+				uint32_t nr_fields;
+				/* Followed by nr_fields struct ustctl_field. */
+			} _struct;
+			struct {
+				uint32_t nr_choices;
+				char tag_name[LTTNG_UST_SYM_NAME_LEN];
+				/* Followed by nr_choices struct ustctl_field. */
+			} variant;
+		} legacy;
 		char padding[USTCTL_UST_TYPE_PADDING];
 	} u;
 } LTTNG_PACKED;

include/lttng/ust-events.h
@@ -88,13 +88,17 @@ enum lttng_client_types {
 /* Update the astract_types name table in lttng-types.c along with this enum */
 enum lttng_abstract_types {
 	atype_integer,
-	atype_enum,
-	atype_array,
-	atype_sequence,
+	atype_enum,	/* legacy */
+	atype_array,	/* legacy */
+	atype_sequence,	/* legacy */
 	atype_string,
 	atype_float,
 	atype_dynamic,
-	atype_struct,
+	atype_struct,	/* legacy */
+	atype_enum_nestable,
+	atype_array_nestable,
+	atype_sequence_nestable,
+	atype_struct_nestable,
 	NR_ABSTRACT_TYPES,
 };
 
@@ -132,17 +136,14 @@ struct lttng_enum_entry {
 	  .atype = atype_integer,				\
 	  .u =							\
 		{						\
-		  .basic = 					\
+		  .integer =					\
 			{					\
-			  .integer =				\
-				{				\
-				  .size = sizeof(_type) * CHAR_BIT,		\
-				  .alignment = lttng_alignof(_type) * CHAR_BIT,	\
-				  .signedness = lttng_is_signed_type(_type),	\
-				  .reverse_byte_order = _byte_order != BYTE_ORDER, \
-				  .base = _base,				\
-				  .encoding = lttng_encode_##_encoding,		\
-				}				\
+			  .size = sizeof(_type) * CHAR_BIT,	\
+			  .alignment = lttng_alignof(_type) * CHAR_BIT,	\
+			  .signedness = lttng_is_signed_type(_type), \
+			  .reverse_byte_order = _byte_order != BYTE_ORDER, \
+			  .base = _base,			\
+			  .encoding = lttng_encode_##_encoding,	\
 			}					\
 		},						\
 	}							\
@@ -172,18 +173,15 @@ struct lttng_integer_type {
 	  .atype = atype_float,					\
 	  .u =							\
 		{						\
-		  .basic =					\
+		  ._float =					\
 			{					\
-			  ._float =				\
-				{				\
-				  .exp_dig = sizeof(_type) * CHAR_BIT		\
-						  - _float_mant_dig(_type),	\
-				  .mant_dig = _float_mant_dig(_type),		\
-				  .alignment = lttng_alignof(_type) * CHAR_BIT,	\
-				  .reverse_byte_order = BYTE_ORDER != FLOAT_WORD_ORDER,	\
-				}				\
+			  .exp_dig = sizeof(_type) * CHAR_BIT	\
+					  - _float_mant_dig(_type), \
+			  .mant_dig = _float_mant_dig(_type),	\
+			  .alignment = lttng_alignof(_type) * CHAR_BIT, \
+			  .reverse_byte_order = BYTE_ORDER != FLOAT_WORD_ORDER,	\
 			}					\
-		},						\
+		}						\
 	}							\
 
 #define LTTNG_UST_FLOAT_TYPE_PADDING	24
@@ -195,20 +193,22 @@ struct lttng_float_type {
 	char padding[LTTNG_UST_FLOAT_TYPE_PADDING];
 };
 
+/* legacy */
 #define LTTNG_UST_BASIC_TYPE_PADDING	128
 union _lttng_basic_type {
-	struct lttng_integer_type integer;
+	struct lttng_integer_type integer;	/* legacy */
 	struct {
 		const struct lttng_enum_desc *desc;	/* Enumeration mapping */
 		struct lttng_integer_type container_type;
-	} enumeration;
+	} enumeration;				/* legacy */
 	struct {
 		enum lttng_string_encodings encoding;
-	} string;
-	struct lttng_float_type _float;
+	} string;				/* legacy */
+	struct lttng_float_type _float;		/* legacy */
 	char padding[LTTNG_UST_BASIC_TYPE_PADDING];
 };
 
+/* legacy */
 struct lttng_basic_type {
 	enum lttng_abstract_types atype;
 	union {
@@ -220,19 +220,48 @@ struct lttng_basic_type {
 struct lttng_type {
 	enum lttng_abstract_types atype;
 	union {
-		union _lttng_basic_type basic;
+		/* provider ABI 2.0 */
+		struct lttng_integer_type integer;
+		struct lttng_float_type _float;
 		struct {
-			struct lttng_basic_type elem_type;
-			unsigned int length;		/* num. elems. */
-		} array;
+			enum lttng_string_encodings encoding;
+		} string;
 		struct {
-			struct lttng_basic_type length_type;
-			struct lttng_basic_type elem_type;
-		} sequence;
+			const struct lttng_enum_desc *desc;	/* Enumeration mapping */
+			struct lttng_type *container_type;
+		} enum_nestable;
 		struct {
-			uint32_t nr_fields;
-			struct lttng_event_field *fields;	/* Array of fields. */
-		} _struct;
+			const struct lttng_type *elem_type;
+			unsigned int length;			/* Num. elems. */
+			unsigned int alignment;
+		} array_nestable;
+		struct {
+			const char *length_name;		/* Length field name. */
+			const struct lttng_type *elem_type;
+			unsigned int alignment;			/* Alignment before elements. */
+		} sequence_nestable;
+		struct {
+			unsigned int nr_fields;
+			const struct lttng_event_field *fields;	/* Array of fields. */
+			unsigned int alignment;
+		} struct_nestable;
+
+		union {
+			/* legacy provider ABI 1.0 */
+			union _lttng_basic_type basic;	/* legacy */
+			struct {
+				struct lttng_basic_type elem_type;
+				unsigned int length;		/* Num. elems. */
+			} array;			/* legacy */
+			struct {
+				struct lttng_basic_type length_type;
+				struct lttng_basic_type elem_type;
+			} sequence;			/* legacy */
+			struct {
+				unsigned int nr_fields;
+				struct lttng_event_field *fields;	/* Array of fields. */
+			} _struct;			/* legacy */
+		} legacy;
 		char padding[LTTNG_UST_TYPE_PADDING];
 	} u;
 };
@@ -258,7 +287,12 @@ struct lttng_event_field {
 	const char *name;
 	struct lttng_type type;
 	unsigned int nowrite;	/* do not write into trace */
-	char padding[LTTNG_UST_EVENT_FIELD_PADDING];
+	union {
+		struct {
+			unsigned int nofilter:1;	/* do not consider for filter */
+		} ext;
+		char padding[LTTNG_UST_EVENT_FIELD_PADDING];
+	} u;
 };
 
 enum lttng_ust_dynamic_type {

include/lttng/ust-tracepoint-event.h
@@ -237,6 +237,11 @@ void __event_template_proto___##_provider##___##_name(_TP_ARGS_DATA_PROTO(_args)
 	  .name = #_item,					\
 	  .type = __type_integer(_type, _byte_order, _base, none),\
 	  .nowrite = _nowrite,					\
+	  .u = {						\
+		  .ext = {					\
+			  .nofilter = 0,			\
+		  },						\
+	  },							\
 	},
 
 #undef _ctf_float
@@ -245,6 +250,11 @@ void __event_template_proto___##_provider##___##_name(_TP_ARGS_DATA_PROTO(_args)
 	  .name = #_item,					\
 	  .type = __type_float(_type),				\
 	  .nowrite = _nowrite,					\
+	  .u = {						\
+		  .ext = {					\
+			  .nofilter = 0,			\
+		  },						\
+	  },							\
 	},
 
 #undef _ctf_array_encoded
@@ -255,17 +265,24 @@ void __event_template_proto___##_provider##___##_name(_TP_ARGS_DATA_PROTO(_args)
 	  .name = #_item,					\
 	  .type =						\
 		{						\
-		  .atype = atype_array,				\
+		  .atype = atype_array_nestable,		\
 		  .u =						\
 			{					\
-			  .array =				\
+			  .array_nestable =			\
 				{				\
-				  .elem_type = __type_integer(_type, _byte_order, _elem_type_base, _encoding), \
+				  .elem_type = __LTTNG_COMPOUND_LITERAL(struct lttng_type, \
+					__type_integer(_type, _byte_order, _elem_type_base, _encoding)), \
 				  .length = _length,		\
+				  .alignment = 0,		\
 				}				\
 			}					\
 		},						\
 	  .nowrite = _nowrite,					\
+	  .u = {						\
+		  .ext = {					\
+			  .nofilter = 0,			\
+		  },						\
+	  },							\
 	},
 
 #undef _ctf_sequence_encoded
@@ -273,20 +290,37 @@ void __event_template_proto___##_provider##___##_name(_TP_ARGS_DATA_PROTO(_args)
 			_length_type, _src_length, _encoding, _nowrite, \
 			_elem_type_base)			\
 	{							\
+	  .name = "_" #_item "_length",				\
+	  .type = __type_integer(_length_type, BYTE_ORDER, 10, none), \
+	  .nowrite = _nowrite,					\
+	  .u = {						\
+		  .ext = {					\
+			  .nofilter = 1,			\
+		  },						\
+	  },							\
+	},							\
+	{							\
 	  .name = #_item,					\
 	  .type =						\
 		{						\
-		  .atype = atype_sequence,			\
+		  .atype = atype_sequence_nestable,		\
 		  .u =						\
 			{					\
-			  .sequence =				\
+			  .sequence_nestable =			\
 				{				\
-				  .length_type = __type_integer(_length_type, BYTE_ORDER, 10, none), \
-				  .elem_type = __type_integer(_type, _byte_order, _elem_type_base, _encoding), \
+				  .length_name = "_" #_item "_length", \
+				  .elem_type = __LTTNG_COMPOUND_LITERAL(struct lttng_type, \
+					__type_integer(_type, _byte_order, _elem_type_base, _encoding)), \
+				  .alignment = 0,		\
 				},				\
 			},					\
 		},						\
 	  .nowrite = _nowrite,					\
+	  .u = {						\
+		  .ext = {					\
+			  .nofilter = 0,			\
+		  },						\
+	  },							\
 	},
 
 #undef _ctf_string
@@ -298,10 +332,15 @@ void __event_template_proto___##_provider##___##_name(_TP_ARGS_DATA_PROTO(_args)
 		  .atype = atype_string,			\
 		  .u =						\
 			{					\
-			  .basic = { .string = { .encoding = lttng_encode_UTF8 } } \
+			  .string = { .encoding = lttng_encode_UTF8 } \
 			},					\
 		},						\
 	  .nowrite = _nowrite,					\
+	  .u = {						\
+		  .ext = {					\
+			  .nofilter = 0,			\
+		  },						\
+	  },							\
 	},
 
 #undef _ctf_enum
@@ -309,24 +348,21 @@ void __event_template_proto___##_provider##___##_name(_TP_ARGS_DATA_PROTO(_args)
 	{							\
 		.name = #_item,					\
 		.type = {					\
-			.atype = atype_enum,			\
+			.atype = atype_enum_nestable,		\
 			.u = {					\
-				.basic = {			\
-					.enumeration = {	\
-						.desc = &__enum_##_provider##_##_name, \
-						.container_type = { \
-							.size = sizeof(_type) * CHAR_BIT, \
-							.alignment = lttng_alignof(_type) * CHAR_BIT, \
-							.signedness = lttng_is_signed_type(_type), \
-							.reverse_byte_order = 0, \
-							.base = 10, \
-							.encoding = lttng_encode_none, \
-						},		\
-					},			\
-				 },				\
+				.enum_nestable = {		\
+					.desc = &__enum_##_provider##_##_name, \
+					.container_type = __LTTNG_COMPOUND_LITERAL(struct lttng_type, \
+								__type_integer(_type, BYTE_ORDER, 10, none)), \
+				},				\
 			},					\
 		},						\
 		.nowrite = _nowrite,				\
+		.u = {						\
+			.ext = {				\
+				.nofilter = 0,			\
+			},					\
+		},						\
 	},
 
 #undef TP_FIELDS

liblttng-ust/lttng-context-cgroup-ns.c
@@ -143,12 +143,12 @@ int lttng_add_cgroup_ns_to_ctx(struct lttng_ctx **ctx)
 	}
 	field->event_field.name = "cgroup_ns";
 	field->event_field.type.atype = atype_integer;
-	field->event_field.type.u.basic.integer.size = sizeof(ino_t) * CHAR_BIT;
-	field->event_field.type.u.basic.integer.alignment = lttng_alignof(ino_t) * CHAR_BIT;
-	field->event_field.type.u.basic.integer.signedness = lttng_is_signed_type(ino_t);
-	field->event_field.type.u.basic.integer.reverse_byte_order = 0;
-	field->event_field.type.u.basic.integer.base = 10;
-	field->event_field.type.u.basic.integer.encoding = lttng_encode_none;
+	field->event_field.type.u.integer.size = sizeof(ino_t) * CHAR_BIT;
+	field->event_field.type.u.integer.alignment = lttng_alignof(ino_t) * CHAR_BIT;
+	field->event_field.type.u.integer.signedness = lttng_is_signed_type(ino_t);
+	field->event_field.type.u.integer.reverse_byte_order = 0;
+	field->event_field.type.u.integer.base = 10;
+	field->event_field.type.u.integer.encoding = lttng_encode_none;
 	field->get_size = cgroup_ns_get_size;
 	field->record = cgroup_ns_record;
 	field->get_value = cgroup_ns_get_value;

commit cf22367fec17f2b2650e637de84fc2f614e4d2ac

Fix: incorrect field accounting for dynamic type
Two field accounting issues were introduced by
commit 218deb69baab "tracepoint: Refactor representation of nested
types" which affect the dynamic types used by Java application contexts.

The structure types should account 1 for the structure per se, in
addition to the nested fields.

The atype_dynamic includes an implicit enumeration, which now has two
fields instead of 1.

This was causing memory corruption issues in the lttng-ust-java-tests.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

liblttng-ust-comm/lttng-ust-comm.c
@@ -779,7 +779,7 @@ ssize_t count_one_type(const struct lttng_type *lt)
 		return 1;
 	case atype_struct:
 		return count_fields_recursive(lt->u.legacy._struct.nr_fields,
-				lt->u.legacy._struct.fields);
+				lt->u.legacy._struct.fields) + 1;
 	case atype_enum_nestable:
 		return count_one_type(lt->u.enum_nestable.container_type) + 1;
 	case atype_array_nestable:
@@ -788,7 +788,7 @@ ssize_t count_one_type(const struct lttng_type *lt)
 		return count_one_type(lt->u.sequence_nestable.elem_type) + 1;
 	case atype_struct_nestable:
 		return count_fields_recursive(lt->u.struct_nestable.nr_fields,
-				lt->u.struct_nestable.fields);
+				lt->u.struct_nestable.fields) + 1;
 
 	case atype_dynamic:
 	{
@@ -801,10 +801,10 @@ ssize_t count_one_type(const struct lttng_type *lt)
 		if (ret)
 			return ret;
 		/*
-		 * One field for enum, one field for variant, and
+		 * Two fields for enum, one field for variant, and
 		 * one field per choice.
 		 */
-		return count_fields_recursive(nr_choices, choices) + 2;
+		return count_fields_recursive(nr_choices, choices) + 3;
 	}
 
 	default: