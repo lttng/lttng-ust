74211da04fac64abca20883d03bab5bdd80cf5af
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2020-06-17 18:40:22 -0400

    Fix: python agent: 'time' has no attribute 'clock'
    The time.clock() function was removed in python 3.8 and is marked as
    deprecated since python 3.3. See PEP 418 for more details [1].
    
    Solution
    =====
    
    When the python version is greater than 3.2 use the
    `time.perf_counter()` function [2]. Otherwise, fall back to
    `time.clock()`.
    
    Introduce a compat module to the lttngust agent package providing the
    `_clock` function.
    
    [1] https://www.python.org/dev/peps/pep-0418/
    [2] https://docs.python.org/3/library/time.html#time.perf_counter
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: I3d6d8b24309d45d43b634dc2a6b4d5dbc12da3aa

--- python-lttngust/lttngust/debug.py
+++ python-lttngust/lttngust/debug.py
@@ -16,6 +16,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 from __future__ import unicode_literals, print_function
+import lttngust.compat
 import time
 import sys
 import os
@@ -30,12 +31,12 @@ if _ENABLE_DEBUG:
     def _pwarning(msg):
         fname = inspect.stack()[1][3]
         fmt = '[{:.6f}] LTTng-UST warning: {}(): {}'
-        print(fmt.format(time.clock(), fname, msg), file=sys.stderr)
+        print(fmt.format(lttngust.compat._clock(), fname, msg), file=sys.stderr)
 
     def _pdebug(msg):
         fname = inspect.stack()[1][3]
         fmt = '[{:.6f}] LTTng-UST debug: {}(): {}'
-        print(fmt.format(time.clock(), fname, msg), file=sys.stderr)
+        print(fmt.format(lttngust.compat._clock(), fname, msg), file=sys.stderr)
 
     _pdebug('debug is enabled')
 else:
