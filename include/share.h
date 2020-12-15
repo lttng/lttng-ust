7812516de06a65d8bbc9e919fe033a4521c9753e
Michael Jeanson <mjeanson@efficios.com>
2020-12-15 14:14:04 -0500

    Hide new 'patient_writev' private symbol
    Change-Id: I708373d94cce6ae99dda82857b927d816edb86a7
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/share.h
+++ include/share.h
@@ -26,8 +26,13 @@
 #include <stdlib.h>
 #include <sys/uio.h>
 
+#include "helper.h"
+
+/* Should be hidden but would break the ABI */
 ssize_t patient_write(int fd, const void *buf, size_t count);
+LTTNG_HIDDEN
 ssize_t patient_writev(int fd, struct iovec *iov, int iovcnt);
+/* Should be hidden but would break the ABI */
 ssize_t patient_send(int fd, const void *buf, size_t count, int flags);
 
 #endif /* _LTTNG_SHARE_H */
