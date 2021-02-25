00e0418ceaa42fb0266f8fb410a8e5f3351c0c88
Michael Jeanson <mjeanson@efficios.com>
2021-02-25 10:33:25 -0500

    Hide private ust-dynamic-type.h symbols
    The major SONAME bump to '1' gives us the opportunity to hide private
    symbols that should never have been visible.
    
    Change-Id: I8a366479704830fe098d425f97a556ed5069d3bc
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/ust-dynamic-type.h
+++ include/ust-dynamic-type.h
@@ -9,9 +9,14 @@
 
 #include <lttng/ust-events.h>
 
+#include "ust-helper.h"
+
+LTTNG_HIDDEN
 int lttng_ust_dynamic_type_choices(size_t *nr_choices,
 		const struct lttng_event_field **choices);
+LTTNG_HIDDEN
 const struct lttng_event_field *lttng_ust_dynamic_type_field(int64_t value);
+LTTNG_HIDDEN
 const struct lttng_event_field *lttng_ust_dynamic_type_tag_field(void);
 
 #endif /* _LTTNG_UST_DYNAMIC_TYPE_H */
