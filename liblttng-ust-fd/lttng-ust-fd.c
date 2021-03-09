f7f0310af616d6baffebc42ce2374947e90f4256
Michael Jeanson <mjeanson@efficios.com>
2021-03-09 12:14:12 -0500

    liblttng-ust-fd doesn't use the internal logging system
    liblttng-ust-fd is a standalone library that only uses liblttng-ust
    public symbols and doesn't currently use the internal logging system. We
    don't need to link against the private convenience library and
    initialize the logging system, clean this up.
    
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: Ifd111722e4034de70f68adc3c84b466e3d490370

--- liblttng-ust-fd/lttng-ust-fd.c
+++ liblttng-ust-fd/lttng-ust-fd.c
@@ -13,16 +13,9 @@
 #include <dlfcn.h>
 
 #include <ust-helper.h>
-#include "usterr-signal-safe.h"
 
 static int (*__lttng_ust_fd_plibc_close)(int fd);
 static int (*__lttng_ust_fd_plibc_fclose)(FILE *stream);
-
-static __attribute__((constructor))
-void _lttng_ust_fd_init(void)
-{
-	ust_err_init();
-}
 
 static
 int _lttng_ust_fd_libc_close(int fd)
