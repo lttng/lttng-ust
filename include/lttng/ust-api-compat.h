2150064431b62bdd4062fca77cf0e3297eca27bd
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2021-05-28 11:23:46 -0400

    Documentation: clarify API backward compatibility comment
    Considering that the ABI (soname major version) and API version can
    evolve independently in the future, remove references to the soname
    major version from the API compatibility documentation.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: Ic632c3d0f0940a689139a8e1d19dc5766382327e

--- include/lttng/ust-api-compat.h
+++ include/lttng/ust-api-compat.h
@@ -10,15 +10,18 @@
 #define _LTTNG_UST_API_COMPAT_H
 
 /*
- * In order to disable compatibility API for a range of soname major
- * versions, define LTTNG_UST_COMPAT_API_VERSION to the oldest major
- * version API for which to provide compatibility.
+ * The compat API version controls backward API compatibility at the
+ * source-code level.
+ *
+ * In order to disable compatibility API for a range of API versions, define
+ * LTTNG_UST_COMPAT_API_VERSION to the oldest API version for which to provide
+ * compatibility.
  *
  * If LTTNG_UST_COMPAT_API_VERSION is undefined, API compatibility for
  * all API versions is provided.
  * If LTTNG_UST_COMPAT_API_VERSION is defined to N, API compatibility
- * for soname N or higher is provided, leaving out (not compiling)
- * compatibility for soname lower than N.
+ * for versions N or higher is provided, thus not defining compatibility macros
+ * for versions lower than N.
  */
 
 #ifndef LTTNG_UST_COMPAT_API_VERSION
