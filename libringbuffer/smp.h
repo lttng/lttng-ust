f1d40e23350c25f90cbe46afd8ca87d9b0a01cab
Michael Jeanson <mjeanson@efficios.com>
2022-07-25 14:21:12 -0400

    fix: num_possible_cpus() with hot-unplugged CPUs
    We rely on sysconf(_SC_NPROCESSORS_CONF) to get the maximum possible
    number of CPUs that can be attached to the system for the lifetime of an
    application. We use this value to allocate an array of per-CPU buffers
    that is indexed by the numerical id of the CPUs.
    
    As such we expect that the highest possible CPU id would be one less
    than the number returned by sysconf(_SC_NPROCESSORS_CONF) which is
    unfortunatly not always the case and can vary across libc
    implementations and versions.
    
    Glibc up to 2.35 will count the number of "cpuX" directories in
    "/sys/devices/system/cpu" which doesn't include CPUS that were
    hot-unplugged.
    
    This information is however provided by the kernel in
    "/sys/devices/system/cpu/possible" in the form of a mask listing all the
    CPUs that could possibly be hot-plugged in the system.
    
    This patch changes the implementation of num_possible_cpus() to first
    try parsing the possible CPU mask to extract the highest possible value
    and if this fails fallback to the previous behavior.
    
    Change-Id: I1a3cb1a446154ec443a391d6689cb7d4165726fd
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- libringbuffer/smp.h
+++ libringbuffer/smp.h
@@ -30,13 +30,53 @@
 #define PER_CPU_MEM_SIZE	4096
 
 extern int __num_possible_cpus;
+
+/*
+ * Get the CPU possible mask string from sysfs.
+ *
+ * buf: the buffer where the mask will be read.
+ * max_bytes: the maximum number of bytes to write in the buffer.
+ *
+ * Returns the number of bytes read or -1 on error.
+ */
+int get_possible_cpu_mask_from_sysfs(char *buf, size_t max_bytes)
+	__attribute__((visibility("hidden")));
+
+/*
+ * Get the number of possible CPUs in the system from either
+ * sysconf(_SC_NPROCESSORS_CONF) or some other mechanism depending on the libc.
+ *
+ * Returns the number of possible CPUs in the system or 0 on error.
+ */
+int get_num_possible_cpus_fallback(void)
+	__attribute__((visibility("hidden")));
+
+/*
+ * Get the number of CPUs from the possible cpu mask.
+ *
+ * pmask: the mask to parse.
+ * len: the len of the mask excluding '\0'.
+ *
+ * Returns the number of possible CPUs from the mask or 0 on error.
+ */
+int get_num_possible_cpus_from_mask(const char *pmask, size_t len)
+	__attribute__((visibility("hidden")));
+
 extern void _get_num_possible_cpus(void);
 
+/*
+ * Returns the total number of CPUs in the system. If the cache is not yet
+ * initialized, get the value from "/sys/devices/system/cpu/possible" or
+ * fallback to sysconf and cache it.
+ *
+ * If all methods fail, don't populate the cache and return 0.
+ */
 static inline
 int num_possible_cpus(void)
 {
-	if (!__num_possible_cpus)
+	if (caa_unlikely(!__num_possible_cpus))
 		_get_num_possible_cpus();
+
 	return __num_possible_cpus;
 }
 
