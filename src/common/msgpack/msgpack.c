d9c66e5afd887aca6f8f914ec469b53109d9bd7a
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2022-09-27 16:14:26 -0400

    Fix: event notification capture error handling
    When the captured fields end up taking more than
    
      PIPE_BUF - sizeof(struct lttng_ust_abi_event_notifier_notification) - 1
    
    bytes of space for the msgpack message, the notification append capture
    fails.
    
    Currently, the result is that the msgpack buffer will contain a (likely
    corrupted) truncated msgpack data.
    
    Handle those overflow errors, and when they are encountered, reset the
    msgpack writer position to skip the problematic captured field entirely.
    
    Change-Id: I7ba1bf06aa72512fc73211a1d8ae6823d0e8d7ff

--- src/common/msgpack/msgpack.c
+++ src/common/msgpack/msgpack.c
@@ -483,6 +483,18 @@ int lttng_msgpack_write_double(struct lttng_msgpack_writer *writer, double value
 	return lttng_msgpack_encode_f64(writer, value);
 }
 
+int lttng_msgpack_save_writer_pos(struct lttng_msgpack_writer *writer, uint8_t **pos)
+{
+	*pos = writer->write_pos;
+	return 0;
+}
+
+int lttng_msgpack_restore_writer_pos(struct lttng_msgpack_writer *writer, uint8_t *pos)
+{
+	writer->write_pos = pos;
+	return 0;
+}
+
 void lttng_msgpack_writer_init(struct lttng_msgpack_writer *writer,
 		uint8_t *buffer, size_t size)
 {
