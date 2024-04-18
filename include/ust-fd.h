26be1417f9b37371024e207c691de87d1ecaa19b
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2024-04-18 11:25:55 -0400

    Add close_range wrapper to liblttng-ust-fd.so
    glibc 2.34 implements close_range(2), which is used by the ssh client
    (amongst others). This needs to be overridden to make sure ssh does not
    close lttng-ust file descriptors.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: Ic4e0046499e1f010395aec71a48316b9d1e9bf3f

--- include/ust-fd.h
+++ include/ust-fd.h
@@ -35,5 +35,7 @@ void lttng_ust_unlock_fd_tracker(void);
 int lttng_ust_safe_close_fd(int fd, int (*close_cb)(int));
 int lttng_ust_safe_fclose_stream(FILE *stream, int (*fclose_cb)(FILE *stream));
 int lttng_ust_safe_closefrom_fd(int lowfd, int (*close_cb)(int));
+int lttng_ust_safe_close_range_fd(unsigned int first, unsigned int last, int flags,
+		int (*close_range_cb)(unsigned int, unsigned int, int));
 
 #endif	/* _LTTNG_UST_FD_H */
--- liblttng-ust-fd/lttng-ust-fd.c
+++ liblttng-ust-fd/lttng-ust-fd.c
@@ -33,6 +33,8 @@
 
 static int (*__lttng_ust_fd_plibc_close)(int fd) = NULL;
 static int (*__lttng_ust_fd_plibc_fclose)(FILE *stream) = NULL;
+static int (*__lttng_ust_fd_plibc_close_range)(unsigned int first,
+		unsigned int last, int flags) = NULL;
 
 /*
  * Use dlsym to find the original libc close() symbol and store it in
@@ -72,6 +74,24 @@ void *_lttng_ust_fd_init_plibc_fclose(void)
 	return __lttng_ust_fd_plibc_fclose;
 }
 
+/*
+ * Use dlsym to find the original libc close_range() symbol and store it
+ * in __lttng_ust_fd_plibc_close_range. The close_range symbol only
+ * appears in glibc 2.34, so it is considered optional.
+ */
+static
+void *_lttng_ust_fd_init_plibc_close_range(void)
+{
+	if (__lttng_ust_fd_plibc_close_range == NULL) {
+		__lttng_ust_fd_plibc_close_range = dlsym(RTLD_NEXT, "close_range");
+
+		if (__lttng_ust_fd_plibc_close_range == NULL)
+			__lttng_ust_fd_plibc_close_range = (void *) LTTNG_UST_DLSYM_FAILED_PTR;
+	}
+
+	return __lttng_ust_fd_plibc_close_range;
+}
+
 static
 void _lttng_ust_fd_ctor(void)
 	__attribute__((constructor));
@@ -85,6 +105,7 @@ void _lttng_ust_fd_ctor(void)
 	 */
 	(void) _lttng_ust_fd_init_plibc_close();
 	(void) _lttng_ust_fd_init_plibc_fclose();
+	(void) _lttng_ust_fd_init_plibc_close_range();
 }
 
 /*
@@ -135,6 +156,30 @@ int fclose(FILE *stream)
 
 	return lttng_ust_safe_fclose_stream(stream,
 			__lttng_ust_fd_plibc_fclose);
+}
+
+/*
+ * Override the libc close_range() symbol with our own, allowing
+ * applications to close arbitrary file descriptors. If the fd is owned
+ * by lttng-ust, return -1, errno=EBADF instead of closing it.
+ *
+ * If dlsym failed to find the original libc close_range() symbol,
+ * return -1, errno=ENOSYS.
+ *
+ * There is a short window before the library constructor has executed where
+ * this wrapper could call dlsym() and thus not be async-signal-safe.
+ */
+int close_range(unsigned int first, unsigned int last, int flags)
+{
+	/*
+	 * We can't retry dlsym here since close is async-signal-safe.
+	 */
+	if (_lttng_ust_fd_init_plibc_close_range() == (void *) LTTNG_UST_DLSYM_FAILED_PTR) {
+		errno = ENOSYS;
+		return -1;
+	}
+
+	return lttng_ust_safe_close_range_fd(first, last, flags, __lttng_ust_fd_plibc_close_range);
 }
 
 #if defined(__sun__) || defined(__FreeBSD__)
