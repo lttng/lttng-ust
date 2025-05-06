beba1242c68bff6d37158069d4ba1ab0e008fa07
Olivier Dion <odion@efficios.com>
2025-05-06 13:32:28 -0400

    common/testpoint: Introduce TESTPOINT macro
    The TESTPOINT macro can be used to mark interesting points in code,
    usually after of before an externally visible side-effect.
    
    It creates a local label with the prefix "lttng_ust_testpoint_". Since
    it's not a global, it is usually stripped off or left with the debug
    symbols.
    
    Change-Id: I0e5c8ca8dea52a9c86087d326a4e628bb4b11d42
    Signed-off-by: Olivier Dion <odion@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ src/common/testpoint.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Copyright (C) 2025 Olivier Dion <odion@efficios.com>
+ */
+
+#ifndef _UST_COMMON_TESTPOINT_H
+#define _UST_COMMON_TESTPOINT_H
+
+/*
+ * A test point is a label to some interesting point in the code flow.
+ *
+ * Typically placed after an observable side-effect, testpoints can be used to
+ * force race conditions to happen with an external debugger.
+ */
+#define TESTPOINT(label)					\
+	__asm__ (".local lttng_ust_testpoint_" label "\n\t"	\
+		"lttng_ust_testpoint_" label "=.")
+
+#endif	/* _UST_COMMON_TESTPOINT_H */
