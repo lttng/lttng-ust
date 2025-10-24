72cc0bd724fc14f46826b02b13d232f56cc9162a
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2025-10-24 10:48:05 -0400

    Fix: ustctl experimental counters
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: I232fedb72bb7c367d6e24bf70c8775a442450e34

--- src/lib/lttng-ust/lttng-ust-comm.c
+++ src/lib/lttng-ust/lttng-ust-comm.c
@@ -1515,13 +1515,11 @@ int handle_message(struct sock_info *sock_info,
 #ifdef CONFIG_LTTNG_UST_EXPERIMENTAL_COUNTER
 	case LTTNG_UST_ABI_COUNTER_EVENT:
 	{
-		ret = copy_phdr(payload, payload_size,
+		ret = copy_payload(payload, payload_size,
 				&var_len_cmd_data);
 
-		if (len < 0) {
-			ret = len;
+		if (ret)
 			goto error;
-		}
 
 		args.counter_event.len = payload_size;
 		if (ops->cmd)
